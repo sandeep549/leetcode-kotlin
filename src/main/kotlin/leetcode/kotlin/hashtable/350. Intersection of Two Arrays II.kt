d40c389189180ee78821e5886db97d39c54e27c1
Sandeep K <sandeepkumar.dev@gmail.com>
2021-02-09 06:17:28 +0530

    350. Intersection of Two Arrays II.kt

--- src/main/kotlin/leetcode/kotlin/hashtable/350. Intersection of Two Arrays II.kt
+++ src/main/kotlin/leetcode/kotlin/hashtable/350. Intersection of Two Arrays II.kt
@@ -1,12 +1,14 @@
 package leetcode.kotlin.hashtable
 
+import kotlin.math.min
+
 private fun intersect(nums1: IntArray, nums2: IntArray): IntArray {
-    var map1 = nums1.toList().groupingBy { it }.eachCount()
-    var map2 = nums2.toList().groupingBy { it }.eachCount()
-    var ans = ArrayList<Int>()
-    for ((k, v) in map1) {
+    val map1 = nums1.toList().groupingBy { it }.eachCount()
+    val map2 = nums2.toList().groupingBy { it }.eachCount()
+    val ans = ArrayList<Int>()
+    map1.forEach { (k, v) ->
         if (map2.containsKey(k)) {
-            for (i in 1..Math.min(v, map2.get(k)!!)) ans.add(k)
+            for (i in 1..min(v, map2[k]!!)) ans.add(k)
         }
     }
     return ans.toIntArray()
@@ -17,14 +19,16 @@ private fun intersect2(nums1: IntArray, nums2: IntArray): IntArray {
     nums2.sort()
     var i = 0
     var j = 0
-    var ans = ArrayList<Int>()
+    val ans = ArrayList<Int>()
     while (i < nums1.size && j < nums2.size) {
-        if (nums1[i] < nums2[j]) i++
-        else if (nums1[i] > nums2[j]) j++
-        else {
-            ans.add(nums1[i])
-            i++
-            j++
+        when {
+            nums1[i] < nums2[j] -> i++
+            nums1[i] > nums2[j] -> j++
+            else -> {
+                ans.add(nums1[i])
+                i++
+                j++
+            }
         }
     }
     return ans.toIntArray()
