6219fce8ceb5642645505feb9e58d951f29cc1b4
Sandeep K <sandeepkumar.dev@gmail.com>
2021-02-10 08:13:34 +0530

    7. Reverse Integer.kt

--- src/main/kotlin/leetcode/kotlin/7. Reverse Integer.kt
+++ src/main/kotlin/leetcode/kotlin/7. Reverse Integer.kt
@@ -4,8 +4,8 @@ private fun reverse(x: Int): Int {
     var x = x
     var res = 0
     while (x != 0) {
-        var tail = x % 10
-        var newres = res * 10 + tail
+        val tail = x % 10
+        val newres = res * 10 + tail
         if ((newres - tail) / 10 != res) return 0
         res = newres
         x /= 10
