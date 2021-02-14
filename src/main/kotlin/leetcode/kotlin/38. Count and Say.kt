6b1453e8011d8e1a8a62b0fe7273fff621a1eb84
Sandeep K <sandeepkumar.dev@gmail.com>
2021-02-14 09:07:04 +0530

    38. Count and Say.kt

---
+++ src/main/kotlin/leetcode/kotlin/38. Count and Say.kt
@@ -0,0 +1,19 @@
+package leetcode.kotlin
+
+private fun countAndSay(n: Int): String {
+    if (n == 1) return "1"
+    var str = countAndSay(n - 1)
+    var ch = ' '
+    var count = 0
+    val ans = StringBuilder()
+    str.forEach { digit ->
+        if (digit != ch) {
+            if (ch != ' ') ans.append(count).append(ch)
+            ch = digit
+            count = 1
+        } else {
+            count++
+        }
+    }
+    return ans.append(count).append(ch).toString()
+}
