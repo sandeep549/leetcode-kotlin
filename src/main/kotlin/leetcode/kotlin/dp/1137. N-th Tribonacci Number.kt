2ed71fdb414140f20f7dd72280237b9921ba6104
Sandeep K <sandeepkumar.dev@gmail.com>
2021-02-15 10:44:33 +0530

    1137. N-th Tribonacci Number.kt

--- src/main/kotlin/leetcode/kotlin/dp/1137. N-th Tribonacci Number.kt
+++ src/main/kotlin/leetcode/kotlin/dp/1137. N-th Tribonacci Number.kt
@@ -7,7 +7,7 @@ private fun tribonacci(n: Int): Int {
     var t1 = 1
     var t2 = 1
     for (i in 3..n) {
-        var s = t2 + t1 + t0
+        val s = t2 + t1 + t0
         t0 = t1
         t1 = t2
         t2 = s
@@ -17,11 +17,13 @@ private fun tribonacci(n: Int): Int {
 
 // dp, top-down
 private fun tribonacci2(n: Int): Int {
-    var dp = IntArray(n + 1) { 0 }
+    val dp = IntArray(n + 1) { 0 }
     fun calc(n: Int): Int {
         if (n <= 0) return 0
         if (n <= 2) return 1
-        if (dp[n] == 0) dp[n] = calc(n - 1) + calc(n - 2) + calc(n - 3)
+        if (dp[n] == 0) {
+            dp[n] = calc(n - 1) + calc(n - 2) + calc(n - 3)
+        }
         return dp[n]
     }
     return calc(n)
