ecca6c8e179615c5269dc1cb9f6a61bd1b711bf8
Sandeep K <sandeepkumar.dev@gmail.com>
2021-02-17 14:07:23 +0530

    234. Palindrome Linked List.kt

--- src/main/kotlin/leetcode/kotlin/linkedlist/234. Palindrome Linked List.kt
+++ src/main/kotlin/leetcode/kotlin/linkedlist/234. Palindrome Linked List.kt
@@ -1,40 +1,49 @@
-package leetcode.kotlin
-
-import leetcode.kotlin.linkedlist.ListNode
+package leetcode.kotlin.linkedlist
 
 // Solution-1
 fun isPalindrome(head: ListNode?): Boolean {
-    if (head == null || head.next == null) return true
+    fun reverse(head: ListNode?): ListNode? {
+        if (head == null) return null
+        var newhead: ListNode? = null
+        var curr: ListNode? = head
+        while (curr != null) {
+            var next = curr.next
+            curr.next = newhead
+            newhead = curr
+            curr = next
+        }
+        return newhead
+    }
+    if (head?.next == null) return true
     var slow: ListNode? = head
     var fast: ListNode? = head
-    while (fast != null && fast.next != null) {
+    while (fast?.next != null) {
         slow = slow!!.next
         fast = fast.next!!.next
     }
     if (fast != null) {
         slow = slow!!.next
     }
-    fast = reverse(slow)
-    slow = head
-    while (fast != null) {
-        if (fast.`val` != slow!!.`val`) return false
+    slow = reverse(slow)
+    fast = head
+    while (slow != null) {
+        if (fast!!.`val` != slow.`val`) return false
         fast = fast.next
         slow = slow.next
     }
     return true
 }
 
-private fun reverse(head: ListNode?): ListNode? {
-    if (head == null) return null
-    var newhead: ListNode? = null
-    var currhead: ListNode? = head
-    while (currhead != null) {
-        var next = currhead.next
-        currhead.next = newhead
-        newhead = currhead
-        currhead = next
+private fun isPalindrome3(head: ListNode?): Boolean {
+    if (head == null) return true
+    var left = head
+    fun rightCheck(node: ListNode?): Boolean { // checks at very node for corresponding node
+        if (node == null) return true
+        val isPal = rightCheck(node.next) && (left!!.`val` == node.`val`)
+        left = left!!.next
+        return isPal
     }
-    return newhead
+    return rightCheck(head)
 }
 
 // Solution-2, tough needs more verification on edge cases to check
