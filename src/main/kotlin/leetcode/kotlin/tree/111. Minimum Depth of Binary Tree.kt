9e071c61b271ac52eef9e56704707cecde393872
Sandeep K <sandeepkumar.dev@gmail.com>
2021-02-21 15:46:48 +0530

    111. Minimum Depth of Binary Tree.kt

--- src/main/kotlin/leetcode/kotlin/tree/111. Minimum Depth of Binary Tree.kt
+++ src/main/kotlin/leetcode/kotlin/tree/111. Minimum Depth of Binary Tree.kt
@@ -1,21 +1,28 @@
 package leetcode.kotlin.tree
 
 private fun minDepth(root: TreeNode?): Int {
-    fun dfs(root: TreeNode): Int {
-        if (root.left == null && root.right == null) return 1
-        var l = root.left?.let { dfs(it) } ?: Int.MAX_VALUE
-        var r = root.right?.let { dfs(it) } ?: Int.MAX_VALUE
-        return minOf(l, r) + 1
-    }
     if (root == null) return 0
-    return dfs(root)
+    val l = minDepth(root.left)
+    val r = minDepth(root.right)
+    return if (l == 0 || r == 0) l + r + 1 else minOf(l, r) + 1
 }
 
+@ExperimentalStdlibApi
 private fun minDepth2(root: TreeNode?): Int {
-    if (root == null) return 0
-    val left = minDepth2(root.left)
-    val right = minDepth2(root.right)
-    return if (left == 0 || right == 0) left + right + 1 else Math.min(left, right) + 1
+    var depth = 0
+    if (root == null) return depth
+    val queue = ArrayDeque<TreeNode>()
+    queue.add(root)
+    while (!queue.isEmpty()) {
+        var width = queue.size
+        while (width > 0) {
+            val node = queue.removeFirst()
+            if (node.left == node.right) return depth + 1
+            node.left?.let { queue.add(it) }
+            node.right?.let { queue.add(it) }
+            width--
+        }
+        depth++
+    }
+    return 0
 }
-
-// todo: try with bfs or iteration
