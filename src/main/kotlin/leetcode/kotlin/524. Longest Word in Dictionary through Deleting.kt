75a22a4485a44eb66fa28ed50f4e15bcecd0ac67
Sandeep K <sandeepkumar.dev@gmail.com>
2021-02-23 07:34:57 +0530

    524. Longest Word in Dictionary through Deleting.kt

---
+++ src/main/kotlin/leetcode/kotlin/524. Longest Word in Dictionary through Deleting.kt
@@ -0,0 +1,18 @@
+package leetcode.kotlin
+
+private fun findLongestWord(s: String, d: List<String>): String {
+    var longest = ""
+    for (word in d) {
+        var i = 0 // index in s
+        var j = 0 // index in word
+        while (i < s.length) {
+            if (j < word.length && word[j] == s[i]) j++
+            i++
+        }
+        if (j == word.length) {
+            if (longest.length == word.length && word < longest) longest = word
+            if (longest.length < word.length) longest = word
+        }
+    }
+    return longest
+}
