59734a003c042af3a7a708c2c8406e60c3e0a4b6
Sandeep K <sandeepkumar.dev@gmail.com>
2021-02-26 09:23:12 +0530

    2. Add Two Numbers.kt

--- src/main/kotlin/leetcode/kotlin/linkedlist/2. Add Two Numbers.kt
+++ src/main/kotlin/leetcode/kotlin/linkedlist/2. Add Two Numbers.kt
@@ -75,3 +75,25 @@ private fun addTwoNumbers3(l1: ListNode?, l2: ListNode?): ListNode? {
     }
     return dummy.next
 }
+
+class Solution {
+    fun ListNode?.value() = this?.`val` ?: 0
+    fun ListNode?.nextNode() = this?.next
+    fun addTwoNumbers(l1: ListNode?, l2: ListNode?): ListNode? {
+        var n1 = l1
+        var n2 = l2
+        var c = 0
+        val dummy = ListNode(0)
+        var d = dummy
+        while (n1 != null || n2 != null || c != 0) {
+            val a = n1.value()
+            val b = n2.value()
+            d.next = ListNode((a + b + c) % 10)
+            d = d.next!!
+            c = (a + b + c) / 10
+            n1 = n1.nextNode()
+            n2 = n2.nextNode()
+        }
+        return dummy.next
+    }
+}
