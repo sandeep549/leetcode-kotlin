31d658cbdba3caf856758e52d66328521450402f
Sandeep K <sandeepkumar.dev@gmail.com>
2021-03-01 09:38:10 +0530

    108. Convert Sorted Array to Binary Search Tree.kt

--- src/main/kotlin/leetcode/kotlin/tree/108. Convert Sorted Array to Binary Search Tree.kt
+++ src/main/kotlin/leetcode/kotlin/tree/108. Convert Sorted Array to Binary Search Tree.kt
@@ -3,7 +3,8 @@ package leetcode.kotlin.tree
 private fun sortedArrayToBST(nums: IntArray): TreeNode? {
     fun makeRoot(l: Int, r: Int): TreeNode? {
         if (l > r) return null
-        var m = l + (r - l + 1) / 2 // +1 plays role in case when length is even and you want the middle on right side
+        var m =
+            l + (r - l + 1) / 2 // +1 plays role in case when length is even and you want the middle on right side
         var root = TreeNode(nums[m])
         root.left = makeRoot(l, m - 1)
         root.right = makeRoot(m + 1, r)
