b93261071f6028aa2ff089b9487b08221c77fd56
Sandeep K <sandeepkumar.dev@gmail.com>
2021-03-04 21:33:01 +0530

    437. Path Sum III.kt

--- src/main/kotlin/leetcode/kotlin/tree/easy/437. Path Sum III.kt
+++ src/main/kotlin/leetcode/kotlin/tree/easy/437. Path Sum III.kt
@@ -37,3 +37,20 @@ private fun pathSum(root: TreeNode?, sum: Int): Int {
     dfs(root)
     return ans
 }
+
+private fun pathSum2(root: leetcode.kotlin.tree.TreeNode?, sum: Int): Int {
+    var count = 0
+    fun dfs(root: leetcode.kotlin.tree.TreeNode?, paths: MutableList<Int>) {
+        root?.let {
+            for (i in paths.indices) paths[i] += it.`val`
+            paths.add(it.`val`)
+            for (pathsum in paths) if (pathsum == sum) count++
+            val copyPath = mutableListOf<Int>()
+            paths.forEach { copyPath.add(it) }
+            dfs(it.left, paths)
+            dfs(it.right, copyPath)
+        }
+    }
+    dfs(root, mutableListOf())
+    return count
+}
