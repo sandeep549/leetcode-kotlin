5e88d6d1198a93fdf173d117109a344bfd9490a4
Sandeep K <sandeepkumar.dev@gmail.com>
2021-03-06 06:21:00 +0530

    102. Binary Tree Level Order Traversal.kt

--- src/main/kotlin/leetcode/kotlin/tree/102. Binary Tree Level Order Traversal.kt
+++ src/main/kotlin/leetcode/kotlin/tree/102. Binary Tree Level Order Traversal.kt
@@ -1,22 +1,27 @@
 package leetcode.kotlin.tree
 
-import java.util.*
-import kotlin.collections.ArrayList
-
+/**
+ * Queue methods
+ * Add() -- adds at tail in ArrayDeque
+ * remove() -- remove from front in ArrayDeque
+ *
+ * offer() -- adds at tail in ArrayDeque
+ * poll() -- remove from front in ArrayDeque
+ */
+@ExperimentalStdlibApi
 private fun levelOrder(root: TreeNode?): List<List<Int>> {
     val res = mutableListOf<MutableList<Int>>()
     if (root == null) return res
     val q = ArrayDeque<TreeNode>()
     q.add(root)
-    while (!q.isEmpty()) {
+    while (q.isNotEmpty()) {
         var size = q.size
-        val list = ArrayList<Int>()
-        while (size > 0) {
-            val node = q.poll()
+        val list = mutableListOf<Int>()
+        while (size-- > 0) {
+            val node = q.removeFirst()
             list.add(node.`val`)
             node.left?.let { q.add(it) }
             node.right?.let { q.add(it) }
-            size--
         }
         res.add(list)
     }
