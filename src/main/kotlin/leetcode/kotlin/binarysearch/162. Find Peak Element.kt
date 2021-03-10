6b09bfa9ac837a4d2efbdb9c34af7a7c3b926c30
Sandeep K <sandeepkumar.dev@gmail.com>
2021-03-10 16:01:46 +0530

    162. Find Peak Element.kt

--- src/main/kotlin/leetcode/kotlin/binarysearch/162. Find Peak Element.kt
+++ src/main/kotlin/leetcode/kotlin/binarysearch/162. Find Peak Element.kt
@@ -6,13 +6,20 @@ fun main() {
     println(findPeakElement(intArrayOf(1, 2)))
 }
 
-private fun findPeakElement(arr: IntArray): Int {
+private fun findPeakElement2(arr: IntArray): Int {
     var l = 0
     var r = arr.lastIndex
     while (l < r) {
-        var m = l + (r - l) / 2
+        val m = l + (r - l) / 2
         if (arr[m] < arr[m + 1]) l = m + 1
         else r = m
     }
     return l
+}
+
+private fun findPeakElement(nums: IntArray): Int {
+    for (i in 0 until nums.lastIndex) {
+        if (nums.get(i) > nums.get(i + 1)) return i
+    }
+    return nums.lastIndex
 }
