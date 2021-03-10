f713ac76563e90e253b3c93f5bc1c05249c184c5
Sandeep K <sandeepkumar.dev@gmail.com>
2021-03-10 16:25:29 +0530

    118. Pascal's Triangle.kt

--- src/main/kotlin/leetcode/kotlin/array/easy/118. Pascal's Triangle.kt
+++ src/main/kotlin/leetcode/kotlin/array/easy/118. Pascal's Triangle.kt
@@ -3,15 +3,15 @@ package leetcode.kotlin.array.easy
 private fun generate(numRows: Int): List<List<Int>> {
     val list = ArrayList<List<Int>>()
     for (row in 1..numRows) {
-        val currrow = ArrayList<Int>()
+        val currow = ArrayList<Int>()
         for (item in 1..row) {
             if (item == 1 || item == row) {
-                currrow.add(1)
+                currow.add(1)
             } else {
-                currrow.add(list.get(row - 2).get(item - 2) + list.get(row - 2).get(item - 1))
+                currow.add(list.get(row - 2).get(item - 2) + list.get(row - 2).get(item - 1))
             }
         }
-        list.add(currrow)
+        list.add(currow)
     }
     return list
 }
