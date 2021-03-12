37f9ee8b5685cf8d59a7228d8bc286c9a4e53137
Sandeep K <sandeepkumar.dev@gmail.com>
2021-03-12 21:28:54 +0530

    322. Coin Change.kt

--- src/main/kotlin/leetcode/kotlin/dp/322. Coin Change.kt
+++ src/main/kotlin/leetcode/kotlin/dp/322. Coin Change.kt
@@ -1,7 +1,7 @@
 package leetcode.kotlin.dp
 
 private fun coinChange(coins: IntArray, amount: Int): Int {
-    var dp = IntArray(amount + 1) { -1 }
+    val dp = IntArray(amount + 1) { -1 }
 
     fun getMinCountForAmount(amount: Int): Int {
         if (amount == 0) return 0
@@ -9,8 +9,8 @@ private fun coinChange(coins: IntArray, amount: Int): Int {
         var min = Int.MAX_VALUE
         for (coin in coins) {
             if (amount - coin >= 0) {
-                var m = getMinCountForAmount(amount - coin)
-                if (m >= 0 && m < min) {
+                val m = getMinCountForAmount(amount - coin)
+                if (m in 0 until min) {
                     min = minOf(min, m + 1)
                 }
             }
@@ -19,29 +19,23 @@ private fun coinChange(coins: IntArray, amount: Int): Int {
         return dp[amount]
     }
 
-    var ans = getMinCountForAmount(amount)
+    val ans = getMinCountForAmount(amount)
     return if (ans < 0) -1 else ans
 }
 
-// private fun coinChange2(coins: IntArray, amount: Int): Int {
-//    var MAX = amount + 1 // We can not have more coins than amount
-//    var dp = Array<Int?>(amount + 1) { null }
-//
-//    fun getMinCoinsFor(amount: Int): Int? {
-//        if (amount < 0) return MAX
-//        if (amount == 0) return 0
-//        if (dp[amount] != null) return dp[amount]
-//        var min = MAX
-//        for (coin in coins) min = minOf(min, 1 + getMinCoinsFor(amount - coin)!!)
-//        dp[amount] = min
-//        return min
-//    }
-//    return if (getMinCoinsFor(amount) == MAX) -1 else amount
-// }
-
-// todo: try to get coin used for min i.e. which coin is used in what count to get minimum
-fun main() {
-    println(coinChange(intArrayOf(1, 2, 5), 11))
-    println(coinChange(intArrayOf(2), 3))
-    println(coinChange(intArrayOf(186, 419, 83, 408), 6249))
+private fun coinChange2(coins: IntArray, amount: Int): Int {
+    if (amount < 1) return 0
+    val dp = IntArray(amount + 1)
+    var sum = 0
+    while (++sum <= amount) {
+        var min = -1
+        for (coin in coins) {
+            if (sum >= coin && dp[sum - coin] != -1) {
+                val temp = dp[sum - coin] + 1
+                min = if (min < 0) temp else if (temp < min) temp else min
+            }
+        }
+        dp[sum] = min
+    }
+    return dp[amount]
 }
