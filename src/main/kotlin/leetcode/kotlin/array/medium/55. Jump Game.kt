4c1f9bd2c791e2662068fb2cf9094e2bd23d3ee2
Sandeep K <sandeepkumar.dev@gmail.com>
2021-03-12 22:08:44 +0530

    55. Jump Game.kt

--- src/main/kotlin/leetcode/kotlin/array/medium/55. Jump Game.kt
+++ src/main/kotlin/leetcode/kotlin/array/medium/55. Jump Game.kt
@@ -1,27 +1,28 @@
 package leetcode.kotlin.array.medium
 
 private fun canJump(nums: IntArray): Boolean {
-    var table = Array<Boolean?>(nums.size) { null }
-    fun canReachLast(i: Int): Boolean {
+    val table = Array<Boolean?>(nums.size) { null }
+    fun jump(i: Int): Boolean {
+        if (i > nums.lastIndex) return false
         if (i == nums.lastIndex) return true
-        if (table[i] != null) return table[i]!!
+        table[i]?.let { return it }
         var res = false
-        for (k in 1..nums[i]) {
-            res = canReachLast(i + k)
+        for (k in nums[i] downTo 1) {
+            res = jump(i + k)
             if (res) break
         }
         table[i] = res
         return res
     }
-    return canReachLast(0)
+    return jump(0)
 }
 
 private fun canJump2(nums: IntArray): Boolean {
-    var goodIndex = nums.size - 1
-    for (i in nums.indices.reversed()) {
-        if (i + nums[i] >= goodIndex) {
-            goodIndex = i
+    var lastPos: Int = nums.lastIndex
+    for (i in nums.lastIndex downTo 0) {
+        if (i + nums[i] >= lastPos) {
+            lastPos = i
         }
     }
-    return goodIndex == 0
+    return lastPos == 0
 }
