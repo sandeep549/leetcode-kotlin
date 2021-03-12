6992930c014d18aa1207e94d519cf5472a09d7da
Sandeep K <sandeepkumar.dev@gmail.com>
2021-03-12 07:30:54 +0530

    78. Subsets.kt

--- src/main/kotlin/leetcode/kotlin/array/medium/78. Subsets.kt
+++ src/main/kotlin/leetcode/kotlin/array/medium/78. Subsets.kt
@@ -23,8 +23,8 @@ private fun subsets(nums: IntArray): List<List<Int>> {
 private fun subsets2(nums: IntArray): List<List<Int>> {
     val output = mutableListOf<MutableList<Int>>()
     val n = nums.size
-
-    for (i in 2.toDouble().pow(n).toInt() until 2.toDouble().pow(n + 1).toInt()) { // generate bitmask, from 0..00 to 1..11
+    for (i in 2.0.pow(n).toInt() until 2.0.pow(n + 1)
+        .toInt()) { // generate bitmask, from 0..00 to 1..11
         val bitmask = Integer.toBinaryString(i).substring(1)
         // append subset corresponding to that bitmask
         val curr = mutableListOf<Int>()
@@ -37,9 +37,9 @@ private fun subsets2(nums: IntArray): List<List<Int>> {
 }
 
 private fun subsets3(nums: IntArray): List<List<Int>> {
-    var ans = mutableListOf<List<Int>>()
+    val ans = mutableListOf<List<Int>>()
     fun backtrack(tmpList: MutableList<Int>, start: Int) {
-        ans.add(tmpList.toList())
+        ans.add(tmpList.toList()) // copy it and add it to answer
         for (i in start..nums.lastIndex) {
             tmpList.add(nums[i])
             backtrack(tmpList, i + 1)
