e9423d61ea47747e4942e86b4ea0a729bbf68960
Sandeep K <sandeepkumar.dev@gmail.com>
2021-03-13 17:17:07 +0530

    15. 3Sum.kt

--- src/main/kotlin/leetcode/kotlin/twopointer/15. 3Sum.kt
+++ src/main/kotlin/leetcode/kotlin/twopointer/15. 3Sum.kt
@@ -5,18 +5,18 @@ private fun threeSum(num: IntArray): List<List<Int>> {
     num.sort()
     val res = mutableListOf<List<Int>>()
     for (i in 0 until num.size - 2) {
-        if (i == 0 || i > 0 && num[i] != num[i - 1]) {
+        if (i == 0 || i > 0 && num[i] != num[i - 1]) { // skip same
             var lo = i + 1
-            var hi = num.size - 1
-            val sum = 0 - num[i]
+            var hi = num.lastIndex
+            val target = 0 - num[i]
             while (lo < hi) {
-                if (num.get(lo) + num.get(hi) == sum) {
+                if (num[lo] + num[hi] == target) {
                     res.add(listOf(num[i], num[lo], num[hi]))
-                    while (lo < hi && num[lo] == num[lo + 1]) lo++
-                    while (lo < hi && num[hi] == num[hi - 1]) hi--
+                    while (lo < hi && num[lo] == num[lo + 1]) lo++ // skip same
+                    while (lo < hi && num[hi] == num[hi - 1]) hi-- // skip same
                     lo++
                     hi--
-                } else if (num[lo] + num[hi] < sum) lo++ else hi--
+                } else if (num[lo] + num[hi] < target) lo++ else hi--
             }
         }
     }
