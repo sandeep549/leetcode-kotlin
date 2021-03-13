79866b3cc4db37ea5acd5e875dd8aeaa6a206002
Sandeep K <sandeepkumar.dev@gmail.com>
2021-03-13 17:28:46 +0530

    34. Find First and Last Position of Element in Sorted Array.kt

--- src/main/kotlin/leetcode/kotlin/binarysearch/34. Find First and Last Position of Element in Sorted Array.kt
+++ src/main/kotlin/leetcode/kotlin/binarysearch/34. Find First and Last Position of Element in Sorted Array.kt
@@ -7,16 +7,15 @@ fun main() {
 private fun searchRange(nums: IntArray, target: Int): IntArray {
     var lo = 0
     var hi: Int = nums.lastIndex
-    var mid = 0
     while (lo < hi) { // find first
-        mid = lo + (hi - lo) / 2
+        val mid = lo + (hi - lo) / 2
         if (nums[mid] < target) lo = mid + 1 else hi = mid
     }
     val first = lo
     // don't reset lo to 0
     hi = nums.lastIndex
     while (lo < hi) { // find last
-        mid = lo + (hi - lo + 1) / 2 // keep mid towards hi side
+        val mid = lo + (hi - lo + 1) / 2 // keep mid towards hi side
         if (nums[mid] <= target) lo = mid else hi = mid - 1
     }
     return if (nums.isEmpty() || nums[lo] != target) intArrayOf(-1, -1) else intArrayOf(
