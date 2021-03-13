6b057df636b89695658f87f3f66e0aa08cc536e2
Sandeep K <sandeepkumar.dev@gmail.com>
2021-03-13 17:17:23 +0530

    113. Path Sum II.kt

--- src/main/kotlin/leetcode/kotlin/tree/113. Path Sum II.kt
+++ src/main/kotlin/leetcode/kotlin/tree/113. Path Sum II.kt
@@ -3,16 +3,16 @@ package leetcode.kotlin.tree
 private fun pathSum(root: TreeNode?, targetSum: Int): List<List<Int>> {
     val res = mutableListOf<MutableList<Int>>()
     fun dfs(root: TreeNode?, path: MutableList<Int>, sum: Int) {
-        root?.let { root ->
-            path.add(root.`val`)
-            var sumHere = sum + root.`val`
-            if (root.left == root.right && sumHere == targetSum) {
+        root?.let { rt ->
+            path.add(rt.`val`)
+            val sumHere = sum + rt.`val`
+            if (rt.left == rt.right && sumHere == targetSum) {
                 res.add(path)
             } else {
                 val list = mutableListOf<Int>()
                 list.addAll(path)
-                root.left?.let { dfs(it, list, sumHere) }
-                root.right?.let { dfs(it, path, sumHere) }
+                rt.left?.let { dfs(it, list, sumHere) }
+                rt.right?.let { dfs(it, path, sumHere) }
             }
         }
     }
