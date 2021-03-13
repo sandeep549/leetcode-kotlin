e66fc10c74787bc800917e4c0f2f8b207626556d
Sandeep K <sandeepkumar.dev@gmail.com>
2021-03-13 20:01:54 +0530

    541. Reverse String II.kt

---
+++ src/main/kotlin/leetcode/kotlin/541. Reverse String II.kt
@@ -0,0 +1,17 @@
+package leetcode.kotlin
+
+private fun reverseStr(s: String, k: Int): String {
+    val a = s.toCharArray()
+    var start = 0
+    while (start < a.size) {
+        var i = start
+        var j = minOf(start + k - 1, a.size - 1)
+        while (i < j) {
+            val tmp = a[i]
+            a[i++] = a[j]
+            a[j--] = tmp
+        }
+        start += 2 * k
+    }
+    return String(a)
+}
