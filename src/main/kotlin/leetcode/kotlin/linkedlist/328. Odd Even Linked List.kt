a8bdd575f52e5240058137f47c4dbb408dab843b
Sandeep K <sandeepkumar.dev@gmail.com>
2021-03-17 09:50:04 +0530

    328. Odd Even Linked List.kt

---
+++ src/main/kotlin/leetcode/kotlin/linkedlist/328. Odd Even Linked List.kt
@@ -0,0 +1,16 @@
+package leetcode.kotlin.linkedlist
+
+private fun oddEvenList(head: ListNode?): ListNode? {
+    if (head == null) return null
+    var odd = head
+    var even = head.next
+    val evenHead = even
+    while (even?.next != null) { // ever time stand on even, odd non-null pair
+        odd!!.next = odd.next!!.next
+        even.next = even.next!!.next
+        odd = odd.next
+        even = even.next
+    }
+    odd!!.next = evenHead
+    return head
+}
