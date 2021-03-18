d77001daff6ea9518866b4a2f9f57ac53917c3c2
Sandeep K <sandeepkumar.dev@gmail.com>
2021-03-18 08:58:15 +0530

    131. Palindrome Partitioning.kt

--- src/main/kotlin/leetcode/kotlin/backtracking/131. Palindrome Partitioning.kt
+++ src/main/kotlin/leetcode/kotlin/backtracking/131. Palindrome Partitioning.kt
@@ -24,3 +24,23 @@ private fun partition(s: String): List<List<String>> {
     dfs(0, mutableListOf())
     return ans
 }
+
+private fun partition2(s: String): List<List<String>> {
+    val ans = mutableListOf<MutableList<String>>()
+    val dp = Array(s.length) { BooleanArray(s.length) { false } }
+    fun dfs(i: Int, list: MutableList<String>) {
+        if (i >= s.length) ans.add(list.toMutableList())
+        var k = 0
+        while (i + k < s.length) {
+            if (s[i] == s[i + k] && (k <= 2 || dp[i + 1][i + k - 1])) {
+                dp[i][i + k] = true
+                list.add(s.substring(i, i + k + 1))
+                dfs(i + k + 1, list)
+                list.removeAt(list.size - 1)
+            }
+            k++
+        }
+    }
+    dfs(0, mutableListOf())
+    return ans
+}
