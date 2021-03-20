07afd5b166fd67dfe3fac59648ad7b13e1ecda5d
Sandeep K <sandeepkumar.dev@gmail.com>
2021-03-20 12:54:22 +0530

    179. Largest Number.kt

---
+++ src/main/kotlin/leetcode/kotlin/179. Largest Number.kt
@@ -0,0 +1,9 @@
+package leetcode.kotlin
+
+private fun largestNumber(nums: IntArray): String {
+    val arr = nums.map { it.toString() } // to string array
+    val res =
+        arr.sortedWith(kotlin.Comparator { o1, o2 -> (o2 + o1).compareTo(o1 + o2) }) // sort it
+    if (res[0] == "0") return "0"
+    return res.fold(StringBuilder(), { acc, i -> acc.append(i) }).toString() // fold it
+}
