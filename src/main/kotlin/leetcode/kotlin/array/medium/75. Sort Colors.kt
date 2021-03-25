44cd708334238b6fc9cfaddc2bea8fcb5ac87b4e
Sandeep K <sandeepkumar.dev@gmail.com>
2021-03-25 07:36:39 +0530

    75. Sort Colors.kt

--- src/main/kotlin/leetcode/kotlin/array/medium/75. Sort Colors.kt
+++ src/main/kotlin/leetcode/kotlin/array/medium/75. Sort Colors.kt
@@ -3,23 +3,40 @@ package leetcode.kotlin.array.medium
 // https://en.wikipedia.org/wiki/Dutch_national_flag_problem
 
 private fun sortColors(nums: IntArray) {
-    var i = 0
-    var j = 0
-    var k = nums.size
-    while (j < k) {
-        when {
-            nums[j] == 0 -> {
-                nums[i] = nums[j].also { nums[j] = nums[i] }
-                i++
-                j++
-            }
-            nums[j] == 2 -> {
-                k--
-                nums[k] = nums[j].also { nums[j] = nums[k] }
-            }
-            else -> {
-                j++
-            }
+    var i = 0 // track first index index of 1 from start
+    var j = 0 // track current index
+    var k = nums.lastIndex // track first index of 2 from end
+    while (j <= k) {
+        if (nums[j] == 0) {
+            nums[i] = nums[j].also { nums[j] = nums[i] } // swap i and j
+            i++
         }
+        if (nums[j] == 2) {
+            nums[k] = nums[j].also { nums[j] = nums[k] }
+            k--
+            j--
+        }
+        j++
+    }
+}
+
+private fun sortColors2(nums: IntArray) {
+    // 1-pass
+    var p1 = 0
+    var p2 = nums.size - 1
+    var index = 0
+    while (index <= p2) {
+        if (nums[index] == 0) {
+            nums[index] = nums[p1]
+            nums[p1] = 0
+            p1++
+        }
+        if (nums[index] == 2) {
+            nums[index] = nums[p2]
+            nums[p2] = 2
+            p2--
+            index--
+        }
+        index++
     }
 }
