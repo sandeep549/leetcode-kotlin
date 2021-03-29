225a68254bbc0da28c32bd212b4fcdea829b6544
Sandeep K <sandeepkumar.dev@gmail.com>
2021-03-29 12:40:14 +0530

    1290. Convert Binary Number in a Linked List to Integer.kt

---
+++ src/main/kotlin/leetcode/kotlin/linkedlist/1290. Convert Binary Number in a Linked List to Integer.kt
@@ -0,0 +1,11 @@
+package leetcode.kotlin.linkedlist
+
+private fun getDecimalValue(head: ListNode?): Int {
+    var result = 0
+    var head = head
+    while (head != null) {
+        result = (result shl 1) xor head.`val`
+        head = head.next
+    }
+    return result
+}
