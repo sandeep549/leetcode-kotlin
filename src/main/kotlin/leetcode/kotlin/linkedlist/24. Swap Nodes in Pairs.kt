71c8af0d7d1e75ac276d2696ecb42b742c081877
Sandeep K <sandeepkumar.dev@gmail.com>
2021-03-31 09:14:11 +0530

    24. Swap Nodes in Pairs.kt

---
+++ src/main/kotlin/leetcode/kotlin/linkedlist/24. Swap Nodes in Pairs.kt
@@ -0,0 +1,16 @@
+package leetcode.kotlin.linkedlist
+
+private fun swapPairs(head: ListNode?): ListNode? {
+    val dummy = ListNode(0)
+    dummy.next = head
+    var current: ListNode? = dummy
+    while (current!!.next != null && current.next!!.next != null) {
+        val first = current.next
+        val second = current.next!!.next
+        first!!.next = second!!.next
+        current.next = second
+        current.next!!.next = first
+        current = current.next!!.next
+    }
+    return dummy.next
+}
