2bd9a0a28f8e52db0eb6add915274a68eb78c8b4
Sandeep K <sandeepkumar.dev@gmail.com>
2021-04-07 08:54:23 +0530

    79. Word Search.kt

--- src/main/kotlin/leetcode/kotlin/array/medium/79. Word Search.kt
+++ src/main/kotlin/leetcode/kotlin/array/medium/79. Word Search.kt
@@ -1,50 +1,54 @@
 package leetcode.kotlin.array.medium
 
 private fun exist(board: Array<CharArray>, word: String): Boolean {
-    fun find(idx: Int, r: Int, c: Int, seen: MutableSet<Pair<Int, Int>>): Boolean {
-        if (idx >= word.length) return true
-        if (r < 0 || r >= board.size || c < 0 || c >= board[0].size || board[r][c] != word[idx]) return false
-
-        seen.add(Pair(r, c))
-        var ret = false
-        var col = intArrayOf(0, 1, 0, -1)
-        var row = intArrayOf(1, 0, -1, 0)
-        for (i in 0..3) {
-            var next = Pair(r + row[i], c + col[i])
-            ret = (!seen.contains(next)) && find(idx + 1, next.first, next.second, seen.toMutableSet().also { it.add(Pair(r, c)) })
-            if (ret) return true
+    val seen = Array(board.size) { BooleanArray(board[0].size) { false } }
+    fun find(cr: Int, cc: Int, i: Int): Boolean {
+        if (i == word.length) return true
+        if (cr >= board.size || cr < 0 || cc >= board[0].size || cc < 0 ||
+            seen[cr][cc] || board[cr][cc] != word[i]
+        ) {
+            return false
         }
-        return false
+
+        seen[cr][cc] = true
+        val left = find(cr, cc - 1, i + 1)
+        val right = find(cr, cc + 1, i + 1)
+        val up = find(cr - 1, cc, i + 1)
+        val down = find(cr + 1, cc, i + 1)
+        seen[cr][cc] = false
+        return left || right || up || down
     }
     for (r in board.indices) {
         for (c in board[0].indices) {
-            if (find(0, r, c, mutableSetOf())) return true
+            if (find(r, c, 0)) return true
         }
     }
     return false
 }
 
 private fun exist2(board: Array<CharArray>, word: String): Boolean {
-    fun find(idx: Int, r: Int, c: Int): Boolean {
-        if (idx >= word.length) return true
-        if (r < 0 || r >= board.size || c < 0 || c >= board[0].size || board[r][c] != word[idx]) return false
-
-        board[r][c] = '*' // mark current as visited
-        var ret = false
-        var col = intArrayOf(0, 1, 0, -1)
-        var row = intArrayOf(1, 0, -1, 0)
-        for (i in 0..3) {
-            var next = Pair(r + row[i], c + col[i])
-            ret = find(idx + 1, next.first, next.second)
-            if (ret) break
+    val seen = Array(board.size) { BooleanArray(board[0].size) { false } }
+    fun find(cr: Int, cc: Int, i: Int): Boolean {
+        if (i == word.length) return true
+        if (cr >= board.size || cr < 0 || cc >= board[0].size || cc < 0 ||
+            seen[cr][cc] || board[cr][cc] != word[i]
+        ) {
+            return false
         }
-        board[r][c] = word[idx] // unmark current cell, re-write original char back in board
-        return ret
+
+        board[cr][cc] = '*' // mark visited
+        val left = find(cr, cc - 1, i + 1)
+        val right = find(cr, cc + 1, i + 1)
+        val up = find(cr - 1, cc, i + 1)
+        val down = find(cr + 1, cc, i + 1)
+        board[cr][cc] = word[i] // re-write original char back
+        return left || right || up || down
     }
     for (r in board.indices) {
         for (c in board[0].indices) {
-            if (find(0, r, c)) return true
+            if (find(r, c, 0)) return true
         }
     }
     return false
 }
+
