bd768796be16471d27e36a950dc428bfe7dafefb
Sandeep K <sandeepkumar.dev@gmail.com>
2021-04-12 19:37:16 +0530

    496. Next Greater Element I.kt

---
+++ src/main/kotlin/leetcode/kotlin/array/medium/496. Next Greater Element I.kt
@@ -0,0 +1,18 @@
+package leetcode.kotlin.array.medium
+
+import java.util.Stack
+
+private fun nextGreaterElement(nums1: IntArray, nums2: IntArray): IntArray {
+    val map = mutableMapOf<Int, Int>()
+    val stack = Stack<Int>()
+    for (num in nums2) {
+        while (!stack.isEmpty() && stack.peek() < num) {
+            map[stack.pop()] = num
+        }
+        stack.push(num)
+    }
+    for (i in nums1.indices) {
+        nums1[i] = map.getOrDefault(nums1[i], -1)
+    }
+    return nums1
+}
