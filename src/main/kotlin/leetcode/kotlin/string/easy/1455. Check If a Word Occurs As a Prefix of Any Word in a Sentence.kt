b8610a720b1decf44326dc63d4b9c74b0dbfb373
Sandeep K <sandeepkumar.dev@gmail.com>
2021-04-14 01:47:18 +0530

    1455. Check If a Word Occurs As a Prefix of Any Word in a Sentence.kt

---
+++ src/main/kotlin/leetcode/kotlin/string/easy/1455. Check If a Word Occurs As a Prefix of Any Word in a Sentence.kt
@@ -0,0 +1,9 @@
+package leetcode.kotlin.string.easy
+
+private fun isPrefixOfWord(sentence: String, searchWord: String): Int {
+    val arr = sentence.split(" ")
+    for (i in arr.indices) {
+        if (arr[i].startsWith(searchWord)) return i + 1
+    }
+    return -1
+}
