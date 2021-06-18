b4f5cce18003c35453938611f22d264b246a18cb
Sandeep K <sandeepkumar.dev@gmail.com>
2021-06-18 12:26:55 +0530

    198. House Robber.kt

--- src/main/kotlin/leetcode/kotlin/dp/198. House Robber.kt
+++ src/main/kotlin/leetcode/kotlin/dp/198. House Robber.kt
@@ -6,24 +6,30 @@ package leetcode.kotlin.dp
  *           f(n-1)         // max amount robbed till n-1
  *           )
  */
-private fun rob(nums: IntArray): Int {
-    val dp = IntArray(nums.size) { -1 }
-    fun robi(i: Int): Int {
+
+private class SolutionDP {
+    var max = 0
+    lateinit var dp: IntArray
+    fun rob(nums: IntArray): Int {
+        dp = IntArray(nums.size) { -1 }
+        robuntil(nums.lastIndex, nums)
+        return dp.last()
+    }
+
+    private fun robuntil(i: Int, nums: IntArray): Int {
         if (i < 0) return 0
-        if (i == 0) return nums[i]
         if (dp[i] == -1) {
-            dp[i] = maxOf(robi(i - 2) + nums[i], robi(i - 1))
+            dp[i] = maxOf(robuntil(i - 2, nums) + nums[i], robuntil(i - 1, nums))
         }
         return dp[i]
     }
-    return robi(nums.lastIndex)
 }
 
 private fun rob2(nums: IntArray): Int {
     var sl = 0 // second last house
     var l = 0 // last house
     for (n in nums) {
-        val cur = Math.max(sl + n, l)
+        val cur = maxOf(sl + n, l)
         sl = l
         l = cur
     }
