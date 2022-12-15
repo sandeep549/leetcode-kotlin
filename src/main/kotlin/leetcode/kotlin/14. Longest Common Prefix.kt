8cbc2bff1c9a5cc5a68a0192b37dd2a9e3b46745
SandeepK <sandeepkumar.dev@gmail.com>
2022-12-15 11:58:49 +0100

    14. Longest Common Prefix.kt

--- src/main/kotlin/leetcode/kotlin/14. Longest Common Prefix.kt
+++ src/main/kotlin/leetcode/kotlin/14. Longest Common Prefix.kt
@@ -7,10 +7,10 @@ fun main() {
 private fun longestCommonPrefix(strs: Array<String>): String {
     val common = StringBuilder()
     if (strs.isEmpty()) return common.toString()
-    for (index in 0..strs[0].lastIndex) {
-        var ch = strs[0][index]
-        for (str in 1..strs.lastIndex) {
-            if (index > strs[str].lastIndex || strs[str][index] != ch) {
+    for (index in 0 until strs[0].length) {
+        val ch = strs[0][index]
+        for (str in 1 until strs.size) {
+            if (index >= strs[str].length || strs[str][index] != ch) {
                 return common.toString()
             }
         }
