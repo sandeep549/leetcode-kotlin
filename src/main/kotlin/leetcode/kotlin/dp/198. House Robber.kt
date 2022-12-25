c991f57a6da1aa970c6b740182d67b5a95c2902f
SandeepK <sandeepkumar.dev@gmail.com>
2022-12-25 10:32:07 +0100

    198. House Robber.kt

--- src/main/kotlin/leetcode/kotlin/dp/198. House Robber.kt
+++ src/main/kotlin/leetcode/kotlin/dp/198. House Robber.kt
@@ -1,9 +1,9 @@
 package leetcode.kotlin.dp
 
 /**
- * f(n) = Max(              // max amount to robbed till n
- *           f(n-2) + A[n], // max amount robbed till n-2 and current house
- *           f(n-1)         // max amount robbed till n-1
+ * f(n) = Max(              // max loot till n
+ *           f(n-2) + A[n], // max loot till n-2 and loot current
+ *           f(n-1)         // max loot till n-1
  *           )
  */
 
@@ -12,26 +12,29 @@ private class SolutionDP {
     lateinit var dp: IntArray
     fun rob(nums: IntArray): Int {
         dp = IntArray(nums.size) { -1 }
-        robuntil(nums.lastIndex, nums)
+        robupto(nums, nums.lastIndex)
         return dp.last()
     }
 
-    private fun robuntil(i: Int, nums: IntArray): Int {
+    private fun robupto(nums: IntArray, i: Int): Int {
         if (i < 0) return 0
         if (dp[i] == -1) {
-            dp[i] = maxOf(robuntil(i - 2, nums) + nums[i], robuntil(i - 1, nums))
+            dp[i] = maxOf(
+                robupto(nums, i - 2) + nums[i],
+                robupto(nums, i - 1)
+            )
         }
         return dp[i]
     }
 }
 
 private fun rob2(nums: IntArray): Int {
-    var sl = 0 // second last house
-    var l = 0 // last house
-    for (n in nums) {
-        val cur = maxOf(sl + n, l)
+    var l = 0
+    var sl = 0
+    for(money in nums) {
+        val here = maxOf(sl + money, l)
         sl = l
-        l = cur
+        l = here
     }
     return l
 }
