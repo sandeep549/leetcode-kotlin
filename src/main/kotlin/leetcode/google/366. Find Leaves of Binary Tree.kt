db666a47a89625bddce93510dedd71e12926b5d7
Sandeep K <sandeepkumar.dev@gmail.com>
2022-12-27 17:17:19 +0100

    366. Find Leaves of Binary Tree.kt

---
+++ src/main/kotlin/leetcode/google/366. Find Leaves of Binary Tree.kt
@@ -0,0 +1,38 @@
+package leetcode.google
+
+import leetcode.kotlin.tree.TreeNode
+
+private fun findLeaves(root: TreeNode?): List<List<Int>> {
+    val map = sortedMapOf<Int, MutableList<Int>>(comparator = Comparator { t1, t2 -> t2-t1 })
+    dfs(root, map)
+    return map.values.toList()
+}
+
+private fun dfs(root: TreeNode?, map: MutableMap<Int, MutableList<Int>>): Int {
+    if(root == null) return 0
+    val l = dfs(root.left, map)
+    val r = dfs(root.right, map)
+    val rank = maxOf(l, r)
+    val list = map.getOrDefault(rank, mutableListOf<Int>())
+    list.add(root.`val`)
+    map[rank] = list
+    return rank
+}
+
+
+private fun findLeaves2(root: TreeNode?): List<List<Int>> {
+    val map = sortedMapOf<Int, MutableList<Int>>()
+    dfs(root, map)
+    return map.values.toList().reversed()
+}
+
+private fun dfs2(root: TreeNode?, map: MutableMap<Int, MutableList<Int>>): Int {
+    if(root == null) return 0
+    val l = dfs(root.left, map)
+    val r = dfs(root.right, map)
+    val rank = maxOf(l, r)
+    val list = map.getOrDefault(rank, mutableListOf<Int>())
+    list.add(root.`val`)
+    map[rank] = list
+    return rank
+}
