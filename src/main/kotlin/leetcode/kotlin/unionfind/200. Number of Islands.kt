e115b5978ac33b99142b2c90a3c1897ea664bad9
Sandeep K <sandeepkumar.dev@gmail.com>
2022-12-27 16:53:57 +0100

    200. Number of Islands.kt

--- src/main/kotlin/leetcode/kotlin/unionfind/200. Number of Islands.kt
+++ src/main/kotlin/leetcode/kotlin/unionfind/200. Number of Islands.kt
@@ -1,76 +1,54 @@
 package leetcode.kotlin.unionfind
 
+val seen = mutableSetOf<Pair<Int, Int>>()
 private fun numIslands(grid: Array<CharArray>): Int {
-    val seen = mutableSetOf<Pair<Int, Int>>()
-    fun dfs(r: Int, c: Int): Boolean {
-        if (r < 0 || r >= grid.size || c < 0 || c >= grid[0].size) return false
-        if (grid[r][c] == '0' || seen.contains(Pair(r, c))) return false
-        seen.add(Pair(r, c))
-        dfs(r, c + 1)
-        dfs(r, c - 1)
-        dfs(r - 1, c)
-        dfs(r + 1, c)
-        return true
-    }
-
     var count = 0
     for (r in grid.indices) {
         for (c in grid[0].indices) {
-            if (dfs(r, c)) count++
+            if (dfs(grid, r, c)) count++
         }
     }
     return count
 }
 
-private fun numIslands2(grid: Array<CharArray>): Int {
-    fun dfs(r: Int, c: Int) {
-        if (r < 0 || r >= grid.size || c < 0 || c >= grid[0].size) return
-        if (grid[r][c] == '0') return
-        grid[r][c] = '0'
-        dfs(r, c + 1)
-        dfs(r, c - 1)
-        dfs(r - 1, c)
-        dfs(r + 1, c)
-    }
-
-    var count = 0
-    for (r in grid.indices) {
-        for (c in grid[0].indices) {
-            if (grid[r][c] == '1') {
-                count++
-                dfs(r, c)
-            }
-        }
-    }
-    return count
+fun dfs(grid: Array<CharArray>, r: Int, c: Int): Boolean {
+    if (r < 0 || r >= grid.size || c < 0 || c >= grid[0].size) return false
+    if (grid[r][c] == '0' || seen.contains(Pair(r, c))) return false
+    seen.add(Pair(r, c))
+    dfs(grid, r, c + 1)
+    dfs(grid, r, c - 1)
+    dfs(grid, r - 1, c)
+    dfs(grid, r + 1, c)
+    return true
 }
 
+
+val islands = mutableListOf<MutableSet<Pair<Int, Int>>>()
 private fun numIslands3(grid: Array<CharArray>): Int {
-    val islands = mutableListOf<MutableSet<Pair<Int, Int>>>()
-    fun find(r: Int, c: Int, island: MutableSet<Pair<Int, Int>>) {
-        if (r < 0 || r >= grid.size || c < 0 || c >= grid[0].size) return // invalid index
-        if (grid[r][c] == '0') return // not land
-        val land = Pair(r, c)
-        if (island.contains(land)) return // land part of current island under consideration
-        if (islands.any { it.contains(land) }) return // land part of other islands
-
-        island.add(land) // add this land to current island
-
-        // search further
-        find(r, c + 1, island)
-        find(r, c - 1, island)
-        find(r - 1, c, island)
-        find(r + 1, c, island)
-    }
-
     for (r in grid.indices) {
         for (c in grid[0].indices) {
             val island = mutableSetOf<Pair<Int, Int>>()
-            find(r, c, island)
+            find(grid, r, c, island)
             if (island.isNotEmpty()) islands.add(island)
         }
     }
     return islands.size
+}
+
+fun find(grid: Array<CharArray>, r: Int, c: Int, island: MutableSet<Pair<Int, Int>>) {
+    if (r < 0 || r >= grid.size || c < 0 || c >= grid[0].size) return // invalid index
+    if (grid[r][c] == '0') return // not land
+    val land = Pair(r, c)
+    if (island.contains(land)) return // land part of current island under consideration
+    if (islands.any { it.contains(land) }) return // land part of other islands
+
+    island.add(land) // add this land to current island
+
+    // search further
+    find(grid, r, c + 1, island)
+    find(grid, r, c - 1, island)
+    find(grid, r - 1, c, island)
+    find(grid, r + 1, c, island)
 }
 
 // todo: do it with bfs and union find
