643afaec248abf9c83d99708786b50aab3932496
Sandeep K <sandeepkumar.dev@gmail.com>
2022-12-29 07:25:30 +0100

    2034. Stock Price Fluctuation.t.kt

---
+++ src/main/kotlin/leetcode/google/2034. Stock Price Fluctuation.t.kt
@@ -0,0 +1,35 @@
+package leetcode.google
+
+import java.util.TreeMap
+
+
+private class StockPrice {
+    var priceMap = TreeMap<Int, Int>() // price, value
+    var valueMap = TreeMap<Int, MutableSet<Int>>() // value, time-set map
+
+    fun update(timestamp: Int, price: Int) {
+        if (priceMap.containsKey(timestamp)) {
+            val prevPrice = priceMap[timestamp]!!
+            val timeSet = valueMap[prevPrice]!!
+            timeSet.remove(timestamp)
+            if (timeSet.isEmpty()) {
+                valueMap.remove(prevPrice)
+            }
+        }
+        valueMap.putIfAbsent(price, HashSet())
+        valueMap[price]!!.add(timestamp)
+        priceMap[timestamp] = price
+    }
+
+    fun current(): Int {
+        return priceMap.lastEntry().value
+    }
+
+    fun maximum(): Int {
+        return valueMap.lastKey()
+    }
+
+    fun minimum(): Int {
+        return valueMap.firstKey()
+    }
+}
