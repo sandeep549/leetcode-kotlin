a42896bc7857a2fa0a8c454ba8c393b38070530c
Sandeep K <sandeepkumar.dev@gmail.com>
2022-12-31 18:43:30 +0100

    2101. Detonate the Maximum Bombs.kt

--- src/main/kotlin/leetcode/google/2101. Detonate the Maximum Bombs.kt
+++ src/main/kotlin/leetcode/google/2101. Detonate the Maximum Bombs.kt
@@ -1,5 +1,14 @@
 package leetcode.google
 
+import java.util.LinkedList
+
+import java.util.Queue
+
+
+// ###########################################################################################
+//
+// ###########################################################################################
+
 fun maximumDetonation(bombs: Array<IntArray>): Int {
     var res = 0
     for (i in bombs.indices) {
@@ -10,7 +19,7 @@ fun maximumDetonation(bombs: Array<IntArray>): Int {
     return res
 }
 
-fun maxWithThisExplosion(bombs: Array<IntArray>, index: Int, path: MutableSet<Int>) {
+private fun maxWithThisExplosion(bombs: Array<IntArray>, index: Int, path: MutableSet<Int>) {
     path.add(index)
     val explosion = bombs[index]
     for (i in bombs.indices) {
@@ -30,4 +39,71 @@ private fun inRange(a: IntArray, b: IntArray): Boolean {
 // todo: this range function has some bug
 fun isInRange(explosion: IntArray, bomb: IntArray): Boolean {
     return (bomb[0] - explosion[0]) * (bomb[0] - explosion[0]) + (bomb[1] - explosion[1]) * (bomb[1] - explosion[1]) <= explosion[2] * explosion[2]
+}
+
+// ###########################################################################################
+//
+// ###########################################################################################
+
+private fun maximumDetonation2(bombs: Array<IntArray>): Int {
+    val graph = HashMap<Int, MutableList<Int>>()
+
+    for (i in bombs.indices) {
+        for (j in bombs.indices) {
+            if (i == j) continue
+            if (inRange(bombs[i], bombs[j])) {
+                graph[i]!!.add(j)
+            }
+            if (inRange(bombs[j], bombs[i])) {
+                graph[j]!!.add(i)
+            }
+        }
+    }
+
+    var result = 0
+    for (i in bombs.indices) {
+        result = maxOf(result, dfs(graph, BooleanArray(bombs.size), i))
+    }
+    return result
+}
+
+private fun dfs(graph: Map<Int, MutableList<Int>>, seen: BooleanArray, index: Int): Int {
+    if (seen[index]) return 0
+    seen[index] = true
+    var result = 1
+    for (adjacentNode in graph[index]!!) {
+        result += dfs(graph, seen, adjacentNode)
+    }
+    return result
+}
+
+// ###########################################################################################
+// BFS
+// ###########################################################################################
+
+fun maximumDetonation3(bombs: Array<IntArray>): Int {
+    var max = 0
+    for (i in bombs.indices) {
+        max = maxOf(max, bfs(bombs, i))
+    }
+    return max
+}
+
+private fun bfs(bombs: Array<IntArray>, index: Int): Int {
+    val queue: Queue<Int> = LinkedList()
+    val seen = BooleanArray(bombs.size)
+    seen[index] = true
+    queue.offer(index)
+    var count = 1 // start from 1 since the first added bomb can detonate itself
+    while (!queue.isEmpty()) {
+        val currBomb = queue.poll()
+        for (j in bombs.indices) { //search for bombs to detonate
+            if (!seen[j] && isInRange(bombs[currBomb], bombs[j])) {
+                seen[j] = true
+                count++
+                queue.offer(j)
+            }
+        }
+    }
+    return count
 }
