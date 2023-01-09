a201fb967a044757e6a245573c8f204a2ec5d03e
SandeepK <sandeepkumar.dev@gmail.com>
2023-01-09 08:49:06 +0100

    2244. Minimum Rounds to Complete All Tasks.kt

--- src/main/kotlin/2244. Minimum Rounds to Complete All Tasks.kt
+++ src/main/kotlin/2244. Minimum Rounds to Complete All Tasks.kt
@@ -1,15 +1,26 @@
-
 class Solution2244 {
     fun minimumRounds(tasks: IntArray): Int {
-        val count: HashMap<Int, Int> = HashMap()
-        for (a in tasks) {
-            count[a] = count.getOrDefault(a, 0) + 1
+        val freq: MutableMap<Int, Int> = HashMap()
+        // Store the frequencies in the map.
+        for (task in tasks) {
+            freq[task] = freq.getOrDefault(task, 0) + 1
         }
-        var res = 0
-        for (freq in count.values) {
-            if (freq == 1) return -1
-            res += (freq + 2) / 3
+        var minimumRounds = 0
+        // Iterate over the task's frequencies.
+        for (count in freq.values) {
+            // If the frequency is 1, it's not possible to complete tasks.
+            if (count == 1) {
+                return -1
+            }
+            minimumRounds += if (count % 3 == 0) {
+                // Group all the task in triplets.
+                count / 3
+            } else {
+                // If count % 3 = 1; (count / 3 - 1) groups of triplets and 2 pairs.
+                // If count % 3 = 2; (count / 3) groups of triplets and 1 pair.
+                count / 3 + 1
+            }
         }
-        return res
+        return minimumRounds
     }
 }
