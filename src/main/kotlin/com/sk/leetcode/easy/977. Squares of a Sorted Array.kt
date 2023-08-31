bd050ee7f22a02342b9daaffed5be1241bce2b0a
Sandeep Kumar <130656105+Sandeep-Vonage@users.noreply.github.com>
2023-08-31 14:32:39 +0530

    977. Squares of a Sorted Array.kt

---
+++ src/main/kotlin/com/sk/leetcode/easy/977. Squares of a Sorted Array.kt
@@ -0,0 +1,23 @@
+package com.sk.leetcode.easy
+
+import kotlin.math.abs
+
+class Solution977 {
+    fun sortedSquares(nums: IntArray): IntArray {
+        val ret = IntArray(nums.size)
+        var l = 0
+        var r = nums.size - 1
+        var i = nums.size - 1
+        while (l <= r) {
+            if (abs(nums[l]) > abs(nums[r])) {
+                ret[i] = nums[l] * nums[l]
+                l++
+            } else {
+                ret[i] = nums[r] * nums[r]
+                r--
+            }
+            i--
+        }
+        return ret
+    }
+}
