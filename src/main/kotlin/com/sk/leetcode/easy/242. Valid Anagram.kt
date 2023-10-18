c2f9735b0de39382d2554838431d0a53cabff894
Sandeep Kumar <sandeep.kumar@vonage.com>
2023-10-18 06:24:35 +0530

    242. Valid Anagram.kt

---
+++ src/main/kotlin/com/sk/leetcode/easy/242. Valid Anagram.kt
@@ -0,0 +1,15 @@
+package com.sk.leetcode.easy
+
+class Solution242 {
+    fun isAnagram(s: String, t: String): Boolean {
+        if (s.length != t.length) {
+            return false
+        }
+        val count = IntArray(26)
+        for (i in s.indices) {
+            count[s[i].code - 'a'.code]++
+            count[t[i].code - 'a'.code]--
+        }
+        return count.none { it != 0 }
+    }
+}
