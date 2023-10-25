de3730b842620c046b119f37e49cfcd13006bb2b
SandeepK <sandeepkumar.dev@gmail.com>
2023-10-25 12:05:34 +0530

    104. Maximum Depth of Binary Tree.kt

--- src/main/kotlin/com/sk/topicWise/tree/104. Maximum Depth of Binary Tree.kt
+++ src/main/kotlin/com/sk/topicWise/tree/104. Maximum Depth of Binary Tree.kt
@@ -1,12 +1,13 @@
 package com.sk.topicWise.tree
 
-import kotlin.math.max
 
-private fun maxDepth(root: TreeNode?): Int {
-    root?.let {
-        return max(maxDepth(it.left), maxDepth(it.right)) + 1
+class Solution104 {
+    fun maxDepth(root: TreeNode?): Int {
+        root?.let {
+            return maxOf(maxDepth(it.left), maxDepth(it.right)) + 1
+        }
+        return 0
     }
-    return 0
 }
 
 // todo: do it iteratively
