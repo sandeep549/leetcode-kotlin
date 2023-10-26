2df1a6a4189e764db4a39bf4583d631d27865343
SandeepK <sandeepkumar.dev@gmail.com>
2023-10-26 07:15:53 +0530

    101. Symmetric Tree.kt

--- src/main/kotlin/com/sk/topicWise/tree/101. Symmetric Tree.kt
+++ src/main/kotlin/com/sk/topicWise/tree/101. Symmetric Tree.kt
@@ -2,41 +2,40 @@ package com.sk.topicWise.tree
 
 import java.util.LinkedList
 
-//import kotlin.collections.ArrayDeque
+class Solution101 {
+    fun isSymmetric(root: TreeNode?): Boolean {
+        if (root == null) return true
+        return isMirror(root, root)
+    }
 
-private fun isSymmetric(root: TreeNode?): Boolean {
-    fun isMirror(n1: TreeNode?, n2: TreeNode?): Boolean {
+    private fun isMirror(n1: TreeNode?, n2: TreeNode?): Boolean {
         if (n1 == null && n2 == null) return true
         if (n1 == null || n2 == null) return false
-        return (n1.`val` == n2.`val`)
-            && isMirror(n1.left, n2.right)
-            && isMirror(n1.right, n2.left)
+        return (n1.`val` == n2.`val`) &&
+                isMirror(n1.left, n2.right) &&
+                isMirror(n1.right, n2.left)
     }
-    if (root == null) return true
-    return isMirror(root.left, root.right)
-}
 
-// todo: Leetcode supports Kotlin 1.3.10, which doesn't have ArrayDeque yet
-@ExperimentalStdlibApi
-private fun isSymmetric2(root: TreeNode?): Boolean {
-    if (root == null) return true
-    val queue = ArrayDeque<TreeNode?>()
-    queue.add(root.left)
-    queue.add(root.right)
-    while (!queue.isEmpty()) {
-        val n1 = queue.removeFirst()
-        val n2 = queue.removeFirst()
-        if (n1 == null && n2 == null) continue
-        if (n1 == null || n2 == null) return false
-        if (n1.`val` != n2.`val`) return false
-        with(queue) {
-            add(n1.left)
-            add(n2.right)
-            add(n1.right)
-            add(n2.left)
+    fun isSymmetric2(root: TreeNode?): Boolean {
+        if (root == null) return true
+        val queue = ArrayDeque<TreeNode?>()
+        queue.add(root.left)
+        queue.add(root.right)
+        while (!queue.isEmpty()) {
+            val n1 = queue.removeFirst()
+            val n2 = queue.removeFirst()
+            if (n1 == null && n2 == null) continue
+            if (n1 == null || n2 == null) return false
+            if (n1.`val` != n2.`val`) return false
+            with(queue) {
+                add(n1.left)
+                add(n2.right)
+                add(n1.right)
+                add(n2.left)
+            }
         }
+        return true
     }
-    return true
 }
 
 private fun isSymmetric3(root: TreeNode?): Boolean {
