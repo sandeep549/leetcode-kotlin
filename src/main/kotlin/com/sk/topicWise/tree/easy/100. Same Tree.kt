306740760e5077944456463a11525b5bf34e3fa2
SandeepK <sandeepkumar.dev@gmail.com>
2023-10-26 06:28:01 +0530

    100. Same Tree.kt

--- src/main/kotlin/com/sk/topicWise/tree/easy/100. Same Tree.kt
+++ src/main/kotlin/com/sk/topicWise/tree/easy/100. Same Tree.kt
@@ -2,10 +2,10 @@ package com.sk.topicWise.tree.easy
 
 import com.sk.topicWise.tree.TreeNode
 
-class Solution100_1 {
+class Solution100 {
     fun isSameTree(p: TreeNode?, q: TreeNode?): Boolean {
-        if(p == null && q == null) return true
-        if(p == null || q == null) return false
+        if (p == null && q == null) return true
+        if (p == null || q == null) return false
         return p.`val` == q.`val` &&
                 isSameTree(p.left, q.left) &&
                 isSameTree(p.right, q.right)
