c3b72ed46ad05050a320fa9131b6f6b8307ad7c3
SandeepK <sandeepkumar.dev@gmail.com>
2023-10-29 09:49:03 +0530

    70. Climbing Stairs.kt

--- src/main/kotlin/com/sk/topicWise/dp/70. Climbing Stairs.kt
+++ src/main/kotlin/com/sk/topicWise/dp/70. Climbing Stairs.kt
@@ -1,32 +1,34 @@
 package com.sk.topicWise.dp
 
-fun main() {
-    println(climbStairs(2))
-}
+class Solution70 {
+    // top-down
+    fun climbStairs(n: Int): Int {
+        val dp = IntArray(n + 1) { -1 }
+        return ways(n, dp)
+    }
 
-// top-down using recursion
-private fun climbStairs(n: Int): Int {
-    val dp = IntArray(n + 1)
-    fun ways(n: Int): Int {
-        if (n == 0 || n == 1) return n
-        if (n == 2) return 2
-        if (dp[n] == 0) {
-            dp[n] = ways(n - 1) + ways(n - 2)
+    private fun ways(n: Int, dp: IntArray): Int {
+        if (n == 0 || n == 1) return 1
+        if (dp[n] == -1) {
+            dp[n] = ways(n - 1, dp) + ways(n - 2, dp)
         }
         return dp[n]
     }
-    return ways(n)
+
+    // bottom-up
+    fun climbStairs2(n: Int): Int {
+        // Say we want to reach first step, i.e. 1, then it must result in 1
+        // f(1) = f(0) + f(-1) => f(0) = 1 and f(-1) = 0
+        var a = 0 // ways to reach at curr-2, 0th -1 step
+        var b = 1 // ways to reach at cur-1, 0th step
+        var c = 0 // ways to reach at current step
+        for (i in 1..n) {
+            c = a + b
+            a = b
+            b = c
+        }
+        return c
+    }
+
 }
 
-// bottom-up
-private fun climbStairs2(n: Int): Int {
-    var a = 0
-    var b = 1
-    var c = 0
-    for (i in 1..n) {
-        c = a + b
-        a = b
-        b = c
-    }
-    return c
-}
