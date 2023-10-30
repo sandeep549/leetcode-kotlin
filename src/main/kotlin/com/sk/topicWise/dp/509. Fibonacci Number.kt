8d47245b3372b08828a00a4680506696af686727
SandeepK <sandeepkumar.dev@gmail.com>
2023-10-30 12:24:10 +0530

    509. Fibonacci Number.kt

--- src/main/kotlin/com/sk/topicWise/dp/509. Fibonacci Number.kt
+++ src/main/kotlin/com/sk/topicWise/dp/509. Fibonacci Number.kt
@@ -1,34 +1,37 @@
 package com.sk.topicWise.dp
 
-// recursion
-private fun fib(N: Int): Int {
-    if (N == 0 || N == 1) return N
-    return fib(N - 1) + fib(N - 2)
-}
+class Solution509 {
 
-// dp, top-down
-private fun fib2(N: Int): Int {
-    val dp = IntArray(N + 1)
-    fun recur(n: Int): Int {
+    // dp, bottom-up
+    fun fib(n: Int): Int {
         if (n == 0 || n == 1) return n
-        if (dp[n] == 0) {
-            dp[n] = recur(n - 1) + recur(n - 2)
+        var a = 0
+        var b = 1
+        for (i in 2..n) {
+            val c = a + b
+            a = b
+            b = c
         }
-        return dp[n]
+        return b
     }
-    return recur(N)
+
+    // dp, top-down
+    fun fib2(N: Int): Int {
+        val dp = IntArray(N + 1)
+        fun recur(n: Int): Int {
+            if (n == 0 || n == 1) return n
+            if (dp[n] == 0) {
+                dp[n] = recur(n - 1) + recur(n - 2)
+            }
+            return dp[n]
+        }
+        return recur(N)
+    }
+
+    // recursion
+    fun fib3(N: Int): Int {
+        if (N == 0 || N == 1) return N
+        return fib(N - 1) + fib(N - 2)
+    }
 }
 
-// dp, bottom-up
-private fun fib3(N: Int): Int {
-    if (N == 0) return 0
-    var a = 0
-    var b = 1
-    var c = 1
-    for (i in 2..N) {
-        c = a + b
-        a = b
-        b = c
-    }
-    return c
-}
