7b1b60c015443549fa35b7a5db058800e4dc9b91
SandeepK <sandeepkumar.dev@gmail.com>
2023-10-30 09:06:27 +0530

    300. Longest Increasing Subsequence.kt

--- src/main/kotlin/com/sk/topicWise/dp/medium/300. Longest Increasing Subsequence.kt
+++ src/main/kotlin/com/sk/topicWise/dp/medium/300. Longest Increasing Subsequence.kt
@@ -1,29 +1,23 @@
 package com.sk.topicWise.dp.medium
 
+import kotlin.math.max
+
 /**
  * LIS ending at index i, including index i
  * L(i) = max(L(j)) + 1, where 0<=j<i and arr[j] <= arr[i]
  */
-private fun lengthOfLIS(nums: IntArray): Int {
-    var ans = 1
-    var dp = IntArray(nums.size) { 0 } // min list will be 1 at any index, so 0 initial value is fine
-    fun lis(i: Int): Int { // returns lis ending at index i, including index i
-        if (i == 0) return 1
-        if (dp[i] != 0) return dp[i]
-        var lisHere = 1 // min value for standalone element
-        for (j in 0 until i) {
-            var res = lis(j)
-            if (nums[i] > nums[j] && res + 1 > lisHere) lisHere = res + 1
+
+class Solution300 {
+    fun lengthOfLIS(nums: IntArray): Int {
+        val dp = IntArray(nums.size) { 1 }
+        for (i in nums.indices) {
+            for (j in 0 until i) {
+                if (nums[i] > nums[j] && dp[i] < dp[j] + 1) {
+                    dp[i] = dp[j] + 1
+                }
+            }
         }
-        if (lisHere > ans) ans = lisHere
-        dp[i] = lisHere
-        return lisHere
+        return dp.max()
     }
-    if (nums.isEmpty()) return 0
-    lis(nums.lastIndex)
-    return ans
 }
 
-fun main() {
-    println(lengthOfLIS(intArrayOf(10, 9, 2, 5, 3, 7, 101, 18)))
-}
