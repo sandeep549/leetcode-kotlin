4879eec4f3d388cd06d654cf52727c094e60dd37
SandeepK <sandeepkumar.dev@gmail.com>
2023-11-03 08:55:22 +0530

    102. Binary Tree Level Order Traversal.kt

--- src/main/kotlin/com/sk/topicWise/tree/102. Binary Tree Level Order Traversal.kt
+++ src/main/kotlin/com/sk/topicWise/tree/102. Binary Tree Level Order Traversal.kt
@@ -8,16 +8,15 @@ package com.sk.topicWise.tree
  * offer() -- adds at tail in ArrayDeque
  * poll() -- remove from front in ArrayDeque
  */
-@ExperimentalStdlibApi
 private fun levelOrder(root: TreeNode?): List<List<Int>> {
     val res = mutableListOf<MutableList<Int>>()
-    if (root == null) return res
+    if(root == null) return res
     val q = ArrayDeque<TreeNode>()
     q.add(root)
-    while (q.isNotEmpty()) {
+    while(q.isNotEmpty()) {
         var size = q.size
         val list = mutableListOf<Int>()
-        while (size-- > 0) {
+        while(size-- > 0) {
             val node = q.removeFirst()
             list.add(node.`val`)
             node.left?.let { q.add(it) }
