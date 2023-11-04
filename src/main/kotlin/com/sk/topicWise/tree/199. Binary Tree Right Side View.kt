a5fa1142a9cb5fa943a31f71c3b0265ebb6e6246
SandeepK <sandeepkumar.dev@gmail.com>
2023-11-04 05:56:41 +0530

    199. Binary Tree Right Side View.kt

--- src/main/kotlin/com/sk/topicWise/tree/199. Binary Tree Right Side View.kt
+++ src/main/kotlin/com/sk/topicWise/tree/199. Binary Tree Right Side View.kt
@@ -1,52 +1,35 @@
 package com.sk.topicWise.tree
 
-private fun rightSideView(root: TreeNode?): List<Int> {
-    val list = mutableListOf<Pair<Int, Int>>()
-    val set = mutableSetOf<Int>()
-    fun dfs(root: TreeNode?, level: Int) {
-        root?.let {
-            dfs(root.right, level + 1)
-            if (!set.contains(level)) {
-                list.add(Pair(level, root.`val`))
-                set.add(level)
+class Solution199 {
+    fun rightSideView(root: TreeNode?): List<Int> {
+        val list = mutableListOf<Int>()
+        dfs(root, 0, list)
+        return list
+    }
+
+    private fun dfs(root: TreeNode?, level: Int, list: MutableList<Int>) {
+        if (root == null) return
+        if (level == list.size) list.add(root.`val`)
+        dfs(root.right, level + 1, list)
+        dfs(root.left, level + 1, list)
+    }
+
+    fun rightSideView2(root: TreeNode?): List<Int> { // reverse level traversal
+        val result = mutableListOf<Int>()
+        if (root == null) return result
+        val queue = ArrayDeque<TreeNode>()
+        queue.add(root)
+        while (queue.isNotEmpty()) {
+            val size = queue.size
+            for (i in 0 until size) {
+                val cur = queue.removeFirst()
+                if (i == 0) result.add(cur.`val`)
+                cur.right?.let { queue.add(it) }
+                cur.left?.let { queue.add(it) }
             }
-            dfs(root.left, level + 1)
         }
+        return result
     }
-    dfs(root, 0)
-    list.sortBy { it.first }
-    return list.map { it.second }
+
 }
 
-private fun rightSideView2(root: TreeNode?): List<Int> {
-    val list = mutableListOf<Int>()
-    fun dfs(root: TreeNode?, level: Int) {
-        root?.let {
-            if (list.size < level) list.add(root.`val`)
-            dfs(root.right, level + 1)
-            dfs(root.left, level + 1)
-        }
-    }
-    dfs(root, 1)
-    return list
-}
-
-/*
- Right to left level traversal
- */
-private fun rightSideView3(root: TreeNode?): List<Int> { // reverse level traversal
-    val result = mutableListOf<Int>()
-    val queue = ArrayDeque<TreeNode>()
-    if (root == null) return result
-    queue.add(root)
-    while (queue.isNotEmpty()) {
-        val size = queue.size
-        for (i in 0 until size) {
-            val cur = queue.removeFirst()
-            if (i == 0) result.add(cur.`val`)
-            cur.right?.let { queue.add(it) }
-            cur.left?.let { queue.add(it) }
-        }
-    }
-    return result
-}
