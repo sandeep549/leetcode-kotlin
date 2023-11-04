de448f74335e429a07b95c0f722b158b841d089e
SandeepK <sandeepkumar.dev@gmail.com>
2023-11-04 06:23:13 +0530

    103. Binary Tree Zigzag Level Order Traversal.kt

--- src/main/kotlin/com/sk/topicWise/tree/103. Binary Tree Zigzag Level Order Traversal.kt
+++ src/main/kotlin/com/sk/topicWise/tree/103. Binary Tree Zigzag Level Order Traversal.kt
@@ -1,30 +1,31 @@
 package com.sk.topicWise.tree
 
-@ExperimentalStdlibApi
-private fun zigzagLevelOrder(root: TreeNode?): List<List<Int>> {
-    val res = mutableListOf<MutableList<Int>>()
-    if (root == null) return res
-    var lToR = true
-    var stack = ArrayDeque<TreeNode>()
-    stack.add(root)
-    while (!stack.isEmpty()) {
-        var size = stack.size
-        val level = mutableListOf<Int>()
-        val newStack = ArrayDeque<TreeNode>()
-        while (size-- > 0) {
-            val node = stack.removeLast()
-            level.add(node.`val`)
-            if (lToR) {
-                node.left?.let { newStack.add(it) }
-                node.right?.let { newStack.add(it) }
-            } else {
-                node.right?.let { newStack.add(it) }
-                node.left?.let { newStack.add(it) }
+class Solution103 {
+    fun zigzagLevelOrder(root: TreeNode?): List<List<Int>> {
+        val res = mutableListOf<MutableList<Int>>()
+        if (root == null) return res
+        var lToR = true
+        var stack = ArrayDeque<TreeNode>()
+        stack.add(root)
+        while (!stack.isEmpty()) {
+            val level = mutableListOf<Int>()
+            val newStack = ArrayDeque<TreeNode>()
+            while (stack.isNotEmpty()) {
+                val node = stack.removeLast()
+                level.add(node.`val`)
+                if (lToR) {
+                    node.left?.let { newStack.add(it) }
+                    node.right?.let { newStack.add(it) }
+                } else {
+                    node.right?.let { newStack.add(it) }
+                    node.left?.let { newStack.add(it) }
+                }
             }
+            res.add(level)
+            lToR = !lToR
+            stack = newStack
         }
-        res.add(level)
-        lToR = !lToR
-        stack = newStack
+        return res
     }
-    return res
 }
+
