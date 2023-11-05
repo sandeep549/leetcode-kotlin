9dc88f0da0ad8e45808231f581b4aa7dc90978cc
SandeepK <sandeepkumar.dev@gmail.com>
2023-11-05 19:30:47 +0530

    134. Gas Station.kt

--- src/main/kotlin/com/sk/topicWise/greedy/134. Gas Station.kt
+++ src/main/kotlin/com/sk/topicWise/greedy/134. Gas Station.kt
@@ -1,42 +1,37 @@
 package com.sk.topicWise.greedy
 
-private fun canCompleteCircuit(gas: IntArray, cost: IntArray): Int {
-    var start = 0
-    while (start < gas.size) { // Try with every start point
-        var i = start
-        var g = gas[start]
-        while (true) {
-            i %= gas.size
-            if (g < cost[i]) break // can not reach next station
-            g -= cost[i] // Pay the gas
-            i++ // Go to next station
-            g += gas[i % gas.size] // Take the gas from this station
-            if (i % gas.size == start) return start // reached start point?
+class Solution134 {
+    fun canCompleteCircuit(gas: IntArray, cost: IntArray): Int {
+        if (gas.sum() - cost.sum() < 0) return -1
+        val n = gas.size
+        for (i in gas.indices) { // Try with every start point
+            var j = i
+            var g = gas[i]
+            while (true) {
+                j %= n
+                if (g < cost[j]) break // can not reach next station
+                g -= cost[j] // Pay the gas
+                j++ // Go to next station
+                g += gas[j % n] // Take the gas from this station
+                if (j % n == i) return i // reached start point?
+            }
         }
-        start++
-    }
-    return -1
-}
-
-private fun canCompleteCircuit2(gas: IntArray, cost: IntArray): Int {
-    //determine if we have a solution
-    var total = 0
-    for (i in gas.indices) {
-        total += gas[i] - cost[i]
-    }
-    if (total < 0) {
         return -1
     }
 
-    // find out where to start
-    var tank = 0
-    var start = 0
-    for (i in gas.indices) {
-        tank += gas[i] - cost[i]
-        if (tank < 0) {
-            start = i + 1
-            tank = 0
+    fun canCompleteCircuit2(gas: IntArray, cost: IntArray): Int {
+        if (gas.sum() - cost.sum() < 0) return -1
+        // find out where to start
+        var tank = 0
+        var start = 0
+        for (i in gas.indices) {
+            tank += gas[i] - cost[i]
+            if (tank < 0) {
+                start = i + 1
+                tank = 0
+            }
         }
+        return start
     }
-    return start
 }
+
