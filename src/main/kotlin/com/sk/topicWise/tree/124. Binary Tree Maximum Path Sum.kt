1736fcd882d9b665fe615a6833fddead43cf05e2
SandeepK <sandeepkumar.dev@gmail.com>
2023-11-07 05:25:41 +0530

    124. Binary Tree Maximum Path Sum.kt

--- src/main/kotlin/com/sk/topicWise/tree/124. Binary Tree Maximum Path Sum.kt
+++ src/main/kotlin/com/sk/topicWise/tree/124. Binary Tree Maximum Path Sum.kt
@@ -1,47 +1,39 @@
 package com.sk.topicWise.tree
 
-/**
- * Traverse the tree and while backtrack, maintain mas_so_far at every node, return max passing through
- * current node. We need to return values for first 3 cases from below, 4th case will be considered
- * in max_so_far
- *
- * For each node there can be four ways that the max path goes through the node:
- * 1. Node only
- * 2. Max path through Left Child + Node
- * 3. Max path through Right Child + Node
- * 4. Max path through Left Child + Node + Max path through Right Child
- */
-private fun maxPathSum(root: TreeNode?): Int {
-    var max_so_far = Int.MIN_VALUE
-    fun max(root: TreeNode?): Int {
-        if (root == null) return 0
-        var l = max(root.left)
-        var r = max(root.right)
-        max_so_far = max_so_far.coerceAtLeast(l + root.`val`) // max path till now
-            .coerceAtLeast(r + root.`val`)
-            .coerceAtLeast(l + r + root.`val`)
-            .coerceAtLeast(root.`val`)
-
-        return root.`val`.coerceAtLeast(l + root.`val`) // max passing through this node
-            .coerceAtLeast(r + root.`val`)
-    }
-    max(root)
-    return max_so_far
-}
-
-private fun maxPathSum2(root: TreeNode?): Int {
-    var max_so_far = Int.MIN_VALUE
+class Solution124 {
     /**
-     * Function return max path sum passing through current node
+     * Traverse the tree and while backtrack, maintain mas_so_far at every node, return max passing through
+     * current node. We need to return values for first 3 cases from below, 4th case will be considered
+     * in max_so_far
+     *
+     * For each node there can be four ways that the max path goes through the node:
+     * 1. Node only
+     * 2. Max path through Left Child + Node
+     * 3. Max path through Right Child + Node
+     * 4. Max path through Left Child + Node + Max path through Right Child
      */
-    fun max(root: TreeNode?): Int {
-        if (root == null) return 0
-        // if its negative, it will not add any value to max path, mark it 0
-        var l = Math.max(0, max(root.left))
-        var r = Math.max(0, max(root.right))
-        max_so_far = Math.max(max_so_far, l + r + root.`val`)
-        return Math.max(l, r) + root.`val`
+    fun maxPathSum(root: TreeNode?): Int {
+        return dfs(root).max()
     }
-    max(root)
-    return max_so_far
+
+    private fun dfs(root: TreeNode?): IntArray {
+        if (root == null) return intArrayOf(-1001, -1001)
+        val larr = dfs(root.left)
+        val rarr = dfs(root.right)
+
+        // max path till now
+        val max_so_far = larr[1] // max so far from left side
+            .coerceAtLeast(larr[0] + root.`val`) // max going through curr + left
+            .coerceAtLeast(rarr[1]) // max so far from right side
+            .coerceAtLeast(rarr[0] + root.`val`) // max going through curr + right
+            .coerceAtLeast(root.`val`) // only this node
+            .coerceAtLeast(larr[0] + root.`val` + rarr[0]) // left + node + right
+
+        val max_passing_through = root.`val`
+            .coerceAtLeast(larr[0] + root.`val`)
+            .coerceAtLeast(rarr[0] + root.`val`)
+
+        return intArrayOf(max_passing_through, max_so_far)
+    }
 }
+
