10c0d3820ff16ca3c71c14f2c53c1d4e94129407
SandeepK <sandeepkumar.dev@gmail.com>
2023-11-07 06:10:14 +0530

    173. Binary Search Tree Iterator.kt

--- src/main/kotlin/com/sk/topicWise/tree/bst/173. Binary Search Tree Iterator.kt
+++ src/main/kotlin/com/sk/topicWise/tree/bst/173. Binary Search Tree Iterator.kt
@@ -1,10 +1,44 @@
 package com.sk.topicWise.tree.bst
 
 import com.sk.topicWise.tree.TreeNode
-import java.util.ArrayDeque
 
-private class BSTIterator(root: TreeNode?) {
+/**
+ * Process while calling next()
+ */
+class Solution173(root: TreeNode?) {
+    val s = ArrayDeque<TreeNode>()
 
+    init {
+        push(root)
+    }
+
+    fun next(): Int {
+        return pop()
+    }
+
+    fun hasNext(): Boolean {
+        return s.isNotEmpty()
+    }
+
+    private fun push(n: TreeNode?) {
+        var node = n
+        while (node != null) {
+            s.addLast(node)
+            node = node.left
+        }
+    }
+
+    private fun pop(): Int {
+        val n = s.removeLast()
+        push(n.right)
+        return n.`val`
+    }
+}
+
+/**
+ * Pre-process the tree using recursion.
+ */
+class Solution173_2(root: TreeNode?) {
     private var sortedList = mutableListOf<Int>()
     private var index = -1
 
@@ -13,49 +47,18 @@ private class BSTIterator(root: TreeNode?) {
     }
 
     private fun inroder(node: TreeNode?) {
-        node?.let {
-            inroder(it.left)
-            sortedList.add(it.`val`)
-            inroder(it.right)
-        }
+        if (node == null) return
+        inroder(node.left)
+        sortedList.add(node.`val`)
+        inroder(node.right)
     }
 
-    /** @return the next smallest number */
     fun next(): Int {
         return if (hasNext()) sortedList[++index] else -1
     }
 
-    /** @return whether we have a next smallest number */
     fun hasNext(): Boolean {
         return (index + 1) < sortedList.size
     }
 }
 
-private class BSTIterator2(root: TreeNode?) {
-    private var stack = ArrayDeque<TreeNode>()
-
-    init {
-        traverseLeft(root)
-    }
-
-    private fun traverseLeft(root: TreeNode?) {
-        var curr = root
-        while (curr != null) {
-            stack.push(curr)
-            curr = curr.left
-        }
-    }
-
-    /** @return the next smallest number */
-    fun next(): Int {
-        if (!hasNext()) return -1
-        var top = stack.pop()
-        traverseLeft(top.right)
-        return top.`val`
-    }
-
-    /** @return whether we have a next smallest number */
-    fun hasNext(): Boolean {
-        return stack.size > 1
-    }
-}
