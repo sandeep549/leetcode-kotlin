f30db30697c8b176c9437fc4e304e804d1a50498
SandeepK <sandeepkumar.dev@gmail.com>
2023-11-10 07:16:24 +0530

    33. Search in Rotated Sorted Array.kt

--- src/main/kotlin/com/sk/topicWise/binarysearch/33. Search in Rotated Sorted Array.kt
+++ src/main/kotlin/com/sk/topicWise/binarysearch/33. Search in Rotated Sorted Array.kt
@@ -1,28 +1,30 @@
 package com.sk.topicWise.binarysearch
 
-private fun search(nums: IntArray, target: Int): Int {
-    if (nums.isEmpty()) return -1
+class Solution33 {
+    fun search(nums: IntArray, target: Int): Int {
+        if (nums.isEmpty()) return -1
 
-    // find smallest element, use it's index as rotation count
-    var l = 0
-    var r = nums.lastIndex
-    var mid = 0
-    while (l < r) {
-        mid = l + (r - l) / 2
-        if (nums[mid] > nums[r]) l = mid + 1
-        else r = mid
-    }
+        // find smallest element, use it's index as rotation count
+        var l = 0
+        var r = nums.lastIndex
+        while (l < r) {
+            val mid = l + (r - l) / 2
+            if (nums[mid] > nums[r]) l = mid + 1 // Why comparing with right element and not left ?
+            else r = mid
+        }
 
-    var rot = l
-    l = 0
-    r = nums.lastIndex
-    while (l < r) {
-        mid = l + (r - l) / 2
-        var realmid = (mid + rot) % nums.size
-        if (nums[realmid] < target) l = mid + 1
-        else r = mid
+        val rot = l
+        l = 0
+        r = nums.lastIndex
+        while (l < r) {
+            val mid = l + (r - l) / 2
+            val realmid = (mid + rot) % nums.size
+            if (nums[realmid] < target) l = mid + 1
+            else r = mid
+        }
+        l = (l + rot) % nums.size
+        return if (nums[l] == target) l else -1
     }
-    return if (nums[(l + rot) % nums.size] == target) (l + rot) % nums.size else -1
 }
 
 // from solution
@@ -31,18 +33,17 @@ private fun search2(nums: IntArray, target: Int): Int {
     if (nums.isEmpty()) return -1
     var l = 0
     var r = nums.lastIndex
-    var m = 0
-    var MAX = Int.MAX_VALUE
-    var MIN = Int.MIN_VALUE
+    val MAX = Int.MAX_VALUE
+    val MIN = Int.MIN_VALUE
     while (l < r) {
-        m = l + (r - l) / 2
-        var num = when {
-            nums[m] < nums[0] == target < nums[0] -> nums[m]
+        val mid = l + (r - l) / 2
+        val num = when {
+            nums[mid] < nums[0] == target < nums[0] -> nums[mid]
             target < nums[0] -> MIN
             else -> MAX
         }
-        if (num < target) l = m + 1
-        else r = m
+        if (num < target) l = mid + 1
+        else r = mid
     }
     return if (nums[l] == target) l else -1
 }
