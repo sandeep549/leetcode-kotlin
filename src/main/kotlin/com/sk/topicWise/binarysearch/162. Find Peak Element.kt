69163d11836619eecd3f0d164d2b45cd05dc109e
SandeepK <sandeepkumar.dev@gmail.com>
2023-11-10 05:47:51 +0530

    162. Find Peak Element.kt

--- src/main/kotlin/com/sk/topicWise/binarysearch/162. Find Peak Element.kt
+++ src/main/kotlin/com/sk/topicWise/binarysearch/162. Find Peak Element.kt
@@ -1,25 +1,23 @@
 package com.sk.topicWise.binarysearch
 
-fun main() {
-    println(findPeakElement(intArrayOf(1, 2, 3, 1)))
-    println(findPeakElement(intArrayOf(1, 2, 1, 3, 5, 6, 4)))
-    println(findPeakElement(intArrayOf(1, 2)))
+class Solution162 {
+    fun findPeakElement(arr: IntArray): Int {
+        var l = 0
+        var r = arr.lastIndex
+        while (l < r) {
+            val m = l + (r - l) / 2
+            if (arr[m] < arr[m + 1]) l = m + 1
+            else r = m
+        }
+        return l
+    }
+
+    fun findPeakElement2(nums: IntArray): Int {
+        for (i in 0 until nums.lastIndex) {
+            if (nums[i] > nums[i + 1]) return i
+        }
+        return nums.lastIndex
+    }
+
 }
 
-private fun findPeakElement2(arr: IntArray): Int {
-    var l = 0
-    var r = arr.lastIndex
-    while (l < r) {
-        val m = l + (r - l) / 2
-        if (arr[m] < arr[m + 1]) l = m + 1
-        else r = m
-    }
-    return l
-}
-
-private fun findPeakElement(nums: IntArray): Int {
-    for (i in 0 until nums.lastIndex) {
-        if (nums.get(i) > nums.get(i + 1)) return i
-    }
-    return nums.lastIndex
-}
