fd340bb1f1976c2913b9ed380cd45598b6e79e7c
SandeepK <sandeepkumar.dev@gmail.com>
2023-11-12 07:49:25 +0530

    2. Add Two Numbers.kt

---
+++ src/main/kotlin/com/sk/hard/4. Median of Two Sorted Arrays.kt
@@ -0,0 +1,7 @@
+package com.sk.hard
+
+//class Solution4 {
+//    fun findMedianSortedArrays(nums1: IntArray, nums2: IntArray): Double {
+//
+//    }
+//}
--- src/main/kotlin/com/sk/topicWise/linkedlist/2. Add Two Numbers.kt
+++ src/main/kotlin/com/sk/topicWise/linkedlist/2. Add Two Numbers.kt
@@ -1,19 +1,32 @@
 package com.sk.topicWise.linkedlist
 
-private fun addTwoNumbers(l1: ListNode?, l2: ListNode?): ListNode? {
-    var list1 = l1
-    var list2 = l2
-    var c = 0
-    val dummy = ListNode(0)
-    var d = dummy
-    while (list1 != null || list2 != null || c != 0) {
-        val a = list1?.`val` ?: 0
-        val b = list2?.`val` ?: 0
-        d.next = ListNode((a + b + c) % 10)
-        d = d.next!!
-        c = (a + b + c) / 10
-        if (list1 != null) list1 = list1.next
-        if (list2 != null) list2 = list2.next
+class Solution2 {
+    fun addTwoNumbers(l1: ListNode?, l2: ListNode?): ListNode? {
+        var a = l1
+        var b = l2
+        var c = 0
+        var x = 0
+        var y = 0
+        val dummy = ListNode(0)
+        var curr = dummy
+        while (a != null || b != null || c != 0) {
+            if (a != null) {
+                x = a.`val`
+                a = a.next
+            } else {
+                x = 0
+            }
+            if (b != null) {
+                y = b.`val`
+                b = b.next
+            } else {
+                y = 0
+            }
+            val v = (x + y + c) % 10
+            c = (x + y + c) / 10
+            curr.next = ListNode(v)
+            curr = curr.next!!
+        }
+        return dummy.next
     }
-    return dummy.next
 }
