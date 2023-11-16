0ec8a50977339e11a756eb0140cdec43430489e1
SandeepK <sandeepkumar.dev@gmail.com>
2023-11-16 16:12:22 +0530

    14. Longest Common Prefix.kt

--- src/main/kotlin/com/sk/leetcode/kotlin/14. Longest Common Prefix.kt
+++ src/main/kotlin/com/sk/leetcode/kotlin/14. Longest Common Prefix.kt
@@ -1,20 +1,33 @@
 package com.sk.leetcode.kotlin
 
-fun main() {
-    println(longestCommonPrefix(arrayOf("flow", "flower", "flight")))
-}
 
-private fun longestCommonPrefix(strs: Array<String>): String {
-    val common = StringBuilder()
-    if (strs.isEmpty()) return common.toString()
-    for (index in 0 until strs[0].length) {
-        val ch = strs[0][index]
-        for (str in 1 until strs.size) {
-            if (index >= strs[str].length || strs[str][index] != ch) {
-                return common.toString()
+class Solution14 {
+    fun longestCommonPrefix(strs: Array<String>): String {
+        val common = StringBuilder()
+        if (strs.isEmpty()) return common.toString()
+        for (index in 0 until strs[0].length) {
+            val ch = strs[0][index]
+            for (str in 1 until strs.size) {
+                if (index >= strs[str].length || strs[str][index] != ch) {
+                    return common.toString()
+                }
             }
+            common.append(ch)
         }
-        common.append(ch)
+        return common.toString()
     }
-    return common.toString()
+
+    fun longestCommonPrefix2(v: Array<String>): String {
+        val ans = StringBuilder()
+        v.sort()
+        val first = v[0]
+        val last = v[v.size - 1]
+        for (i in 0 until Math.min(first.length, last.length)) {
+            if (first[i] != last[i]) {
+                return ans.toString()
+            }
+            ans.append(first[i])
+        }
+        return ans.toString()
+    }
 }
