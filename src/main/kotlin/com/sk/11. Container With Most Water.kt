3ff559ac2cd31c953062333b1de06b1db89b6ee5
SandeepK <sandeepkumar.dev@gmail.com>
2023-11-18 05:30:42 +0530

    11. Container With Most Water.kt

--- src/main/kotlin/com/sk/11. Container With Most Water.kt
+++ src/main/kotlin/com/sk/11. Container With Most Water.kt
@@ -1,14 +1,16 @@
 package com.sk
 
-private fun maxArea(height: IntArray): Int {
-    var max = 0
-    var l = 0
-    var r = height.lastIndex
-    while (l < r) {
-        val curr = (r - l) * minOf(height[l], height[r])
-        max = maxOf(max, curr)
-        if (height[l] <= height[r]) l++
-        else r--
+class Solution11 {
+    fun maxArea(height: IntArray): Int {
+        var max = 0
+        var l = 0
+        var r = height.lastIndex
+        while (l < r) {
+            val curr = (r - l) * minOf(height[l], height[r])
+            max = maxOf(max, curr)
+            if (height[l] <= height[r]) l++
+            else r--
+        }
+        return max
     }
-    return max
 }
