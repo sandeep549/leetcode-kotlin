fdc3632f79c97ba4dcea387e908fac978a5babf8
SandeepK <sandeepkumar.dev@gmail.com>
2023-11-21 06:48:58 +0530

    57. Insert Interval.kt

---
+++ src/main/kotlin/com/sk/medium/57. Insert Interval.kt
@@ -0,0 +1,21 @@
+package com.sk.medium
+
+class Solution57 {
+    fun insert(intervals: Array<IntArray>, newInterval: IntArray): Array<IntArray> {
+        val res = ArrayList<IntArray>()
+        var newInterval = newInterval
+        for (interval in intervals) {
+            if (interval[1] < newInterval[0]) {         // current interval is before new one
+                res.add(interval)
+            } else if (newInterval[1] < interval[0]) {  // current interval is after new one
+                res.add(newInterval)
+                res.add(interval)
+                newInterval = intArrayOf(1_00_001, 1_00_001)
+            } else {                                    // Current & new interval overlap
+                newInterval = intArrayOf(minOf(newInterval[0], interval[0]), maxOf(newInterval[1], interval[1]))
+            }
+        }
+        if (newInterval[0] != 1_00_001) res.add(newInterval)
+        return res.toTypedArray()
+    }
+}
