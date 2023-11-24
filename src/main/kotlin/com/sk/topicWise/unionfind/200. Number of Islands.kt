00e1fb82b90be85aba7dd868227bda956fc608c6
SandeepK <sandeepkumar.dev@gmail.com>
2023-11-24 05:50:00 +0530

    200. Number of Islands.kt

--- src/main/kotlin/com/sk/topicWise/unionfind/200. Number of Islands.kt
+++ src/main/kotlin/com/sk/topicWise/unionfind/200. Number of Islands.kt
@@ -1,55 +1,133 @@
 package com.sk.topicWise.unionfind
 
-val seen = mutableSetOf<Pair<Int, Int>>()
-private fun numIslands(grid: Array<CharArray>): Int {
+import java.util.*
+
+
+class Solution200 {
+
+    fun numIslands(grid: Array<CharArray>): Int {
+        val seen = HashSet<Pair<Int, Int>>()
+        var count = 0
+        for (r in grid.indices) {
+            for (c in grid[0].indices) {
+                if (dfs(grid, r, c, seen)) count++
+            }
+        }
+        return count
+    }
+
+    fun dfs(grid: Array<CharArray>, r: Int, c: Int, seen: HashSet<Pair<Int, Int>>): Boolean {
+        if (r < 0 || r >= grid.size || c < 0 || c >= grid[0].size) return false
+        if (grid[r][c] == '0' || seen.contains(Pair(r, c))) return false
+        seen.add(Pair(r, c))
+        dfs(grid, r, c + 1, seen)
+        dfs(grid, r, c - 1, seen)
+        dfs(grid, r - 1, c, seen)
+        dfs(grid, r + 1, c, seen)
+        return true
+    }
+
+
+    var distance = arrayOf(intArrayOf(1, 0), intArrayOf(-1, 0), intArrayOf(0, 1), intArrayOf(0, -1))
+    fun numIslands2(grid: Array<CharArray>): Int {
+        if (grid.isEmpty()) return 0
+        val uf = UnionFind(grid)
+        val rows = grid.size
+        val cols = grid[0].size
+        for (i in 0 until rows) {
+            for (j in 0 until cols) {
+                if (grid[i][j] == '1') {
+                    for (d in distance) {
+                        val x = i + d[0]
+                        val y = j + d[1]
+                        if (x in 0..<rows && y >= 0 && y < cols && grid[x][y] == '1') {
+                            val id1 = i * cols + j
+                            val id2 = x * cols + y
+                            uf.union(id1, id2)
+                        }
+                    }
+                }
+            }
+        }
+        return uf.count
+    }
+}
+
+
+private class UnionFind(grid: Array<CharArray>) {
+    var parent: IntArray
+    var m: Int
+    var n: Int
     var count = 0
-    for (r in grid.indices) {
-        for (c in grid[0].indices) {
-            if (dfs(grid, r, c)) count++
+
+    init {
+        m = grid.size
+        n = grid[0].size
+        parent = IntArray(m * n)
+        for (i in 0 until m) {
+            for (j in 0 until n) {
+                if (grid[i][j] == '1') {
+                    val id = i * n + j
+                    parent[id] = id
+                    count++
+                }
+            }
         }
     }
-    return count
-}
 
-fun dfs(grid: Array<CharArray>, r: Int, c: Int): Boolean {
-    if (r < 0 || r >= grid.size || c < 0 || c >= grid[0].size) return false
-    if (grid[r][c] == '0' || seen.contains(Pair(r, c))) return false
-    seen.add(Pair(r, c))
-    dfs(grid, r, c + 1)
-    dfs(grid, r, c - 1)
-    dfs(grid, r - 1, c)
-    dfs(grid, r + 1, c)
-    return true
-}
-
-
-val islands = mutableListOf<MutableSet<Pair<Int, Int>>>()
-private fun numIslands3(grid: Array<CharArray>): Int {
-    for (r in grid.indices) {
-        for (c in grid[0].indices) {
-            val island = mutableSetOf<Pair<Int, Int>>()
-            find(grid, r, c, island)
-            if (island.isNotEmpty()) islands.add(island)
+    fun union(node1: Int, node2: Int) {
+        val find1 = find(node1)
+        val find2 = find(node2)
+        if (find1 != find2) {
+            parent[find1] = find2
+            count--
         }
     }
-    return islands.size
+
+    fun find(node: Int): Int {
+        if (parent[node] == node) {
+            return node
+        }
+        parent[node] = find(parent[node])
+        return parent[node]
+    }
+
 }
 
-fun find(grid: Array<CharArray>, r: Int, c: Int, island: MutableSet<Pair<Int, Int>>) {
-    if (r < 0 || r >= grid.size || c < 0 || c >= grid[0].size) return // invalid index
-    if (grid[r][c] == '0') return // not land
-    val land = Pair(r, c)
-    if (island.contains(land)) return // land part of current island under consideration
-    if (islands.any { it.contains(land) }) return // land part of other islands
 
-    island.add(land) // add this land to current island
+class Solution200_1 {
+    lateinit var parent: IntArray
+    fun numIslands(grid: Array<CharArray>): Int {
+        if (grid.isEmpty()) return 0
+        val n = grid.size
+        val m = grid[0].size
+        parent = IntArray(m * n)
+        Arrays.fill(parent, -1)
+        for (i in 0 until n) {
+            for (j in 0 until m) {
+                if (grid[i][j] == '1') {
+                    parent[i * m + j] = i * m + j // note, that `parent` was filled witn -1 values
+                    if (i > 0 && grid[i - 1][j] == '1') union(i * m + j, (i - 1) * m + j) // union current+top
+                    if (j > 0 && grid[i][j - 1] == '1') union(i * m + j, i * m + (j - 1)) // union current+left
+                }
+            }
+        }
+        val set = HashSet<Int>()
+        for (k in parent.indices) {
+            if (parent[k] != -1) set.add(find(k))
+        }
+        return set.size
+    }
 
-    // search further
-    find(grid, r, c + 1, island)
-    find(grid, r, c - 1, island)
-    find(grid, r - 1, c, island)
-    find(grid, r + 1, c, island)
+    fun find(x: Int): Int {
+        if (parent[x] == x) return x
+        parent[x] = find(parent[x])
+        return parent[x]
+    }
+
+    fun union(x: Int, y: Int) {
+        val px = find(x)
+        val py = find(y)
+        parent[px] = parent[py]
+    }
 }
-
-// todo: do it with bfs and union find
-// https://leetcode.com/problems/number-of-islands/solution/
