6a2454a683e5e09b8058c2a3ab6b5476f1e6ccb1
SandeepK <sandeepkumar.dev@gmail.com>
2023-12-01 06:20:41 +0530

    22. Generate Parentheses.kt

--- src/main/kotlin/com/sk/topicWise/string/medium/22. Generate Parentheses.kt
+++ src/main/kotlin/com/sk/topicWise/string/medium/22. Generate Parentheses.kt
@@ -1,12 +1,30 @@
 package com.sk.topicWise.string.medium
 
-fun main() {
-    println(generateParenthesis2(3))
-}
+class Solution22 {
 
-private fun generateParenthesis(n: Int): List<String> {
-    val ans = mutableListOf<String>()
-    val arr = CharArray(2 * n)
+    fun generateParenthesis(n: Int): List<String> {
+        val ans = ArrayList<String>()
+        val arr = CharArray(2 * n)
+
+        generate(0, arr, ans)
+        return ans
+    }
+
+    fun generate(pos: Int, arr: CharArray, ans: ArrayList<String>) {
+        // Check if its valid at last and add to result
+        if (pos == arr.size) {
+            if (isValid(arr)) {
+                ans.add(String(arr))
+            }
+        } else {
+            // We have 2 choices at every position
+            arr[pos] = '('
+            generate(pos + 1, arr, ans)
+            arr[pos] = ')'
+            generate(pos + 1, arr, ans)
+        }
+    }
+
     fun isValid(arr: CharArray): Boolean {
         var count = 0
         for (ch in arr) {
@@ -17,32 +35,20 @@ private fun generateParenthesis(n: Int): List<String> {
         return count == 0
     }
 
-    fun generate(pos: Int) {
-        if (pos == arr.size) {
-            if (isValid(arr)) {
-                ans.add(String(arr))
-            }
-        } else {
-            arr[pos] = '('
-            generate(pos + 1)
-            arr[pos] = ')'
-            generate(pos + 1)
-        }
+    fun generateParenthesis2(n: Int): List<String> {
+        val ans = ArrayList<String>()
+        backtrack("", 0, 0, n, ans)
+        return ans
     }
-    generate(0)
-    return ans
-}
 
-private fun generateParenthesis2(n: Int): List<String?> {
-    val ans = mutableListOf<String>()
-    fun backtrack(cur: String, open: Int, close: Int) {
+    fun backtrack(cur: String, open: Int, close: Int, n: Int, ans: ArrayList<String>) {
         if (cur.length == 2 * n) {
             ans.add(cur)
             return
         }
-        if (open < n) backtrack("$cur(", open + 1, close)
-        if (close < open) backtrack("$cur)", open, close + 1)
+        // we have 2 choices here but with valid preconditions
+        if (open < n) backtrack("$cur(", open + 1, close, n, ans)
+        if (close < open) backtrack("$cur)", open, close + 1, n, ans)
     }
-    backtrack("", 0, 0)
-    return ans
+
 }
