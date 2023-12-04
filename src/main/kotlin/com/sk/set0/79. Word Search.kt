4867019f314a3f63d51121b4d12557185a4e3d4a
SandeepK <sandeepkumar.dev@gmail.com>
2023-12-04 06:19:08 +0530

    79. Word Search.kt

--- src/main/kotlin/com/sk/set0/79. Word Search.kt
+++ src/main/kotlin/com/sk/set0/79. Word Search.kt
@@ -1,54 +1,34 @@
 package com.sk.set0
 
-private fun exist(board: Array<CharArray>, word: String): Boolean {
-    val seen = Array(board.size) { BooleanArray(board[0].size) { false } }
-    fun find(cr: Int, cc: Int, i: Int): Boolean {
+class Solution79 {
+    fun exist(board: Array<CharArray>, word: String): Boolean {
+        for (r in board.indices) {
+            for (c in board[0].indices) {
+                if (find(board, word, 0, r, c)) return true
+            }
+        }
+        return false
+    }
+
+    private fun find(
+        board: Array<CharArray>,
+        word: String, i: Int, r: Int, c: Int
+    ): Boolean {
         if (i == word.length) return true
-        if (cr >= board.size || cr < 0 || cc >= board[0].size || cc < 0 ||
-            seen[cr][cc] || board[cr][cc] != word[i]
-        ) {
+        if (r !in board.indices || c !in 0..<board[0].size) return false
+
+        if (board[r][c] == word[i]) {
+            board[r][c] = '*' // mark visited
+            val left = find(board, word, i + 1, r, c - 1)
+            val right = find(board, word, i + 1, r, c + 1)
+            val up = find(board, word, i + 1, r - 1, c)
+            val down = find(board, word, i + 1, r + 1, c)
+            board[r][c] = word[i] // restore
+            return left || right || up || down
+        } else {
             return false
         }
-
-        seen[cr][cc] = true
-        val left = find(cr, cc - 1, i + 1)
-        val right = find(cr, cc + 1, i + 1)
-        val up = find(cr - 1, cc, i + 1)
-        val down = find(cr + 1, cc, i + 1)
-        seen[cr][cc] = false
-        return left || right || up || down
     }
-    for (r in board.indices) {
-        for (c in board[0].indices) {
-            if (find(r, c, 0)) return true
-        }
-    }
-    return false
 }
 
-private fun exist2(board: Array<CharArray>, word: String): Boolean {
-    val seen = Array(board.size) { BooleanArray(board[0].size) { false } }
-    fun find(cr: Int, cc: Int, i: Int): Boolean {
-        if (i == word.length) return true
-        if (cr >= board.size || cr < 0 || cc >= board[0].size || cc < 0 ||
-            seen[cr][cc] || board[cr][cc] != word[i]
-        ) {
-            return false
-        }
-
-        board[cr][cc] = '*' // mark visited
-        val left = find(cr, cc - 1, i + 1)
-        val right = find(cr, cc + 1, i + 1)
-        val up = find(cr - 1, cc, i + 1)
-        val down = find(cr + 1, cc, i + 1)
-        board[cr][cc] = word[i] // re-write original char back
-        return left || right || up || down
-    }
-    for (r in board.indices) {
-        for (c in board[0].indices) {
-            if (find(r, c, 0)) return true
-        }
-    }
-    return false
-}
 
