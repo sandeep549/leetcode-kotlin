cd6019d0abcde38654830baaaef22325f18cb83e
SandeepK <sandeepkumar.dev@gmail.com>
2023-12-05 05:37:05 +0530

    215. Kth Largest Element in an Array.kt

--- src/main/kotlin/com/sk/leetcode/kotlin/215. Kth Largest Element in an Array.kt
+++ src/main/kotlin/com/sk/leetcode/kotlin/215. Kth Largest Element in an Array.kt
@@ -2,18 +2,21 @@ package leetcode.kotlin.array.easy
 
 import java.util.PriorityQueue
 
-private fun findKthLargest(nums: IntArray, k: Int): Int {
-    nums.sort()
-    return nums[nums.size - k]
-}
-
-private fun findKthLargest2(nums: IntArray, k: Int): Int {
-    val priorityQueue = PriorityQueue<Int> { i1, i2 -> i1 - i2 }
-    for (num in nums) {
-        priorityQueue.add(num)
-        if (priorityQueue.size > k) priorityQueue.poll()
+class Solution215 {
+    fun findKthLargest(nums: IntArray, k: Int): Int {
+        nums.sort()
+        return nums[nums.size - k]
     }
-    return priorityQueue.poll()
+
+    fun findKthLargest2(nums: IntArray, k: Int): Int {
+        val priorityQueue = PriorityQueue<Int> { i1, i2 -> i1 - i2 }
+        for (num in nums) {
+            priorityQueue.add(num)
+            if (priorityQueue.size > k) priorityQueue.poll()
+        }
+        return priorityQueue.poll()
+    }
+
 }
 
 // todo: check quick select algorithms for O(n) result
