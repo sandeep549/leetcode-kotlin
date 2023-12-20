82c34184e41f844808c74ffe1e2c1d40c239d4ef
SandeepK <sandeepkumar.dev@gmail.com>
2023-12-20 07:41:46 +0530

    415. Add Strings.kt

--- src/main/kotlin/com/sk/topicWise/string/415. Add Strings.kt
+++ src/main/kotlin/com/sk/topicWise/string/415. Add Strings.kt
@@ -1,16 +1,21 @@
 package com.sk.topicWise.string
 
-private fun addStrings(num1: String, num2: String): String {
-    var i = num1.length - 1
-    var j = num2.length - 1
-    var r = 0
-    var c = 0
-    var ans: String = ""
-    while (i >= 0 || j >= 0 || c > 0) {
-        if (i >= 0) c += num1[i--] - '0'
-        if (j >= 0) c += num2[j--] - '0'
-        ans += (c % 10)
-        c = c / 10
+import java.lang.StringBuilder
+
+class Solution415 {
+    fun addStrings(num1: String, num2: String): String {
+        var i = num1.length - 1
+        var j = num2.length - 1
+        var c = 0
+        val ans = StringBuilder()
+        val n1 = num1.toCharArray()
+        val n2 = num2.toCharArray()
+        while (i >= 0 || j >= 0 || c > 0) {
+            if (i >= 0) c += n1[i--] - '0'
+            if (j >= 0) c += n2[j--] - '0'
+            ans.append(c % 10)
+            c /= 10
+        }
+        return ans.reverse().toString()
     }
-    return ans.reversed()
 }
