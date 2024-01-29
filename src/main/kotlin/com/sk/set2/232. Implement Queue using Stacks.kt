f9fa2884f69d800855bb8d2b552f7ed6427c97f6
SandeepK <sandeepkumar.dev@gmail.com>
2024-01-29 06:51:07 +0530

    232. Implement Queue using Stacks.kt

---
+++ src/main/kotlin/com/sk/set2/232. Implement Queue using Stacks.kt
@@ -0,0 +1,30 @@
+package com.sk.set2
+
+class MyQueue232() {
+
+    val s1 = ArrayDeque<Int>()
+    val s2 = ArrayDeque<Int>()
+
+    fun push(x: Int) {
+        s1.addLast(x)
+    }
+
+    fun pop(): Int {
+        peek()
+        return s2.removeLast()
+    }
+
+    fun peek(): Int {
+        if(s2.isEmpty()) {
+            while(s1.isNotEmpty()) {
+                s2.addLast(s1.removeLast())
+            }
+        }
+        return s2.last()
+    }
+
+    fun empty(): Boolean {
+        return s1.isEmpty() && s2.isEmpty()
+    }
+
+}
