893a271984e8b9dd8a11a88869149f782eed537f
SandeepK <sandeepkumar.dev@gmail.com>
2024-02-15 06:33:37 +0530

    516. Longest Palindromic Subsequence.kt

--- src/main/kotlin/com/sk/topicWise/dp/516. Longest Palindromic Subsequence.kt
+++ src/main/kotlin/com/sk/topicWise/dp/516. Longest Palindromic Subsequence.kt
@@ -17,59 +17,21 @@ class Solution516 {
             }
             return dp[l][r]
         }
-        dp[0][s.length-1] = find(0, s.length-1)
-        return dp[0][s.length-1]
+        return find(0, s.length-1)
     }
 
     fun longestPalindromeSubseq2(s: String): Int {
         val dp = Array(s.length) { IntArray(s.length) }
-        for (l in s.indices.reversed()) {
-            dp[l][l] = 1
-            for (r in l+1 until s.length) {
-                if (s[l] == s[r]) {
-                    dp[l][r] = dp[l+1][r-1] + 2
+        for (i in s.lastIndex downTo 0) {
+            dp[i][i] = 1
+            for (j in i+1 until s.length) {
+                if (s[i] == s[j]) {
+                    dp[i][j] = dp[i+1][j-1] + 2
                 } else {
-                    dp[l][r] = maxOf(dp[l][r-1], dp[l+1][r])
+                    dp[i][j] = maxOf(dp[i][j-1], dp[i+1][j])
                 }
             }
         }
         return dp[0][s.length-1]
-    }
-}
-
-class Solution {
-    fun maxPalindromesAfterOperations(words: Array<String>): Int {
-        val arr = IntArray(26)
-        for(word in words) {
-            for(ch in word.toCharArray()) {
-                arr[ch-'a']++
-            }
-        }
-        words.sortBy { it.length }
-
-        var ans = 0
-        for(word in words) {
-            var len = word.length
-            for(i in arr.indices) {
-                if(arr[i] == 0) continue
-                if(len % 2 == 0 && arr[i] % 2 == 0) {
-                    if(arr[i] >= len) {
-                        arr[i] -= len
-                        len = 0
-                    } else {
-                        len -= arr[i]
-                        arr[i] = 0
-                    }
-                } else if(len % 2 != 0 && arr[i] % 2 != 0){
-                    arr[i] -= 1
-                    len--
-                }
-                if(len == 0) break
-            }
-            if(len == 0) {
-                ans++
-            }
-        }
-        return ans
     }
 }
