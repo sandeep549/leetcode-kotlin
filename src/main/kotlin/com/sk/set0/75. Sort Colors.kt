9539bdb41750046483d479b73dd5dbe76aedb494
SandeepK <sandeepkumar.dev@gmail.com>
2024-06-12 07:27:17 +0530

    75. Sort Colors.kt

--- src/main/kotlin/com/sk/set0/75. Sort Colors.kt
+++ src/main/kotlin/com/sk/set0/75. Sort Colors.kt
@@ -2,41 +2,29 @@ package com.sk.set0
 
 // https://en.wikipedia.org/wiki/Dutch_national_flag_problem
 
-private fun sortColors(nums: IntArray) {
-    var i = 0 // track first index index of 1 from start
-    var j = 0 // track current index
-    var k = nums.lastIndex // track first index of 2 from end
-    while (j <= k) {
-        if (nums[j] == 0) {
-            nums[i] = nums[j].also { nums[j] = nums[i] } // swap i and j
-            i++
-        }
-        if (nums[j] == 2) {
-            nums[k] = nums[j].also { nums[j] = nums[k] }
-            k--
-            j--
-        }
-        j++
-    }
-}
+class Solution75 {
 
-private fun sortColors2(nums: IntArray) {
-    // 1-pass
-    var p1 = 0
-    var p2 = nums.size - 1
-    var index = 0
-    while (index <= p2) {
-        if (nums[index] == 0) {
-            nums[index] = nums[p1]
-            nums[p1] = 0
-            p1++
+    fun sortColors(nums: IntArray) {
+        var i = 0 // track first index of 1 from start, where we want to put next 0
+        var j = 0 // track current index
+        var k = nums.size - 1 // track first index of 2 from end where we want to put next 2
+        while (j <= k) {
+            when (nums[j]) {
+                0 -> { // j has red
+                    nums[i] = nums[j].also { nums[j] = nums[i] }
+                    i++
+                    j++
+                }
+
+                1 -> { // j has white
+                    j++
+                }
+
+                else -> { // j has blue
+                    nums[j] = nums[k].also { nums[k] = nums[j] }
+                    k--
+                }
+            }
         }
-        if (nums[index] == 2) {
-            nums[index] = nums[p2]
-            nums[p2] = 2
-            p2--
-            index--
-        }
-        index++
     }
 }
