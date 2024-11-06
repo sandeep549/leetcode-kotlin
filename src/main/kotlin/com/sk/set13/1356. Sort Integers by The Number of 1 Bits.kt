d510cf980641fe99b7471a248d7835545c22308b
Sandeep Kumar <sandeep.kumar.dev@gmail.com>
2024-11-06 17:18:03 +0530

    1356. Sort Integers by The Number of 1 Bits.kt

---
+++ src/main/kotlin/com/sk/set13/1356. Sort Integers by The Number of 1 Bits.kt
@@ -0,0 +1,20 @@
+package com.sk.set13
+
+class Solution1356 {
+    fun sortByBits(arr: IntArray): IntArray {
+        return arr.sortedWith(Comparator { o1, o2 ->
+            val b1 = o1.countOneBits()
+            val b2 = o2.countOneBits()
+            if (b1 == b2) o1.compareTo(o2)
+            else b1.compareTo(b2)
+        }).toIntArray()
+
+//        arr.toTypedArray().sortWith(Comparator { o1, o2 ->
+//            val b1 = o1.countOneBits()
+//            val b2 = o2.countOneBits()
+//            if (b1 == b2) o1.compareTo(o2)
+//            else b1.compareTo(b2)
+//        })
+//        return arr
+//    }
+    }
