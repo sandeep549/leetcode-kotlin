176a2c3a0d2e4802aedd7561ec83ff079a60470f
Sandeep Kumar <sandeep.kumar.dev@gmail.com>
2025-01-09 07:20:17 +0530

    3042. Count Prefix and Suffix Pairs I.kt

--- src/main/kotlin/com/sk/set30/3042. Count Prefix and Suffix Pairs I.kt
+++ src/main/kotlin/com/sk/set30/3042. Count Prefix and Suffix Pairs I.kt
@@ -2,17 +2,17 @@ package com.sk.set30
 
 class Solution3042 {
     class TrieNode {
-        val children: HashMap<Int, TrieNode> = HashMap()
+        val children = HashMap<Int, TrieNode>()
         var count: Int = 0
     }
 
-    fun countPrefixSuffixPairs(words: Array<String>): Int {
+    fun countPrefixSuffixPairs(words: Array<String>): Long {
         val root = TrieNode()
-        var res = 0
+        var res = 0L
         for (word in words) {
             var x = root
-            for (i in 0 until word.length) {
-                val key = word[i].toInt() * 128 + word[word.length - i - 1].toInt()
+            for (i in word.indices) {
+                val key = word[i].code * 128 + word[word.length - i - 1].code
                 x = x.children.computeIfAbsent(key) { TrieNode() }
                 res += x.count
             }
